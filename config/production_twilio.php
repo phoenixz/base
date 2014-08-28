7865f0fe0efb1ca6bca843edd01ea36df35eed11
Sven Oostenbrink <so.oostenbrink@gmail.com>
2014-08-28 17:31:01 -0500

    Doh

--- config/production_twilio.php
+++ config/production_twilio.php
@@ -1,4 +1,4 @@
 <?php
-$_CONFIG['twilio'] = array('accounts_id'    => 'AC4fda01bbe263bc6642c1d341893836f6',
-                           'accounts_token' => '67e7c3230226c933a84b2bee131b12b7');
+$_CONFIG['twilio'] = array('accounts_id'    => '',
+                           'accounts_token' => '');
 ?>
