d2550c74540403f95f45c0e663dcbb56de0feaa7
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-02-26 20:01:04 -0600

    Fixed 0.19.1 init file, fixed s_ function definitions

--- init/framework/0.19.1.php
+++ init/framework/0.19.1.php
@@ -2,11 +2,16 @@
 /*
  * Create the default error and developers
  */
+sql_query('ALTER TABLE `notifications_classes` CHANGE COLUMN `createdby` `createdby` INT(11) NULL');
+sql_query('ALTER TABLE `notifications_members` CHANGE COLUMN `createdby` `createdby` INT(11) NULL');
+
 if(!sql_get('SELECT `id` FROM `notifications_classes` WHERE `name` = "error"', 'id')){
-    script_exec('notifications/classes create name error methods email,sms description "All error messages will go to this class"');
+    script_exec('base/notifications/classes', 'create name error methods email,sms description "All error messages will go to this class"');
+    script_exec('base/notifications/members', 'add error 1');
 }
 
 if(!sql_get('SELECT `id` FROM `notifications_classes` WHERE `name` = "developers"', 'id')){
-    script_exec('notifications/classes create name developers methods email,sms description "All developer messages will go to this class"');
+    script_exec('base/notifications/classes', 'create name developers methods email,sms description "All developer messages will go to this class"');
+    script_exec('base/notifications/members', 'add developers 1');
 }
 ?>
