ca49fc4fa9e9f70c4961860953a9ba61e86093ca
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-02-27 11:57:52 -0600

    Fixed admin page issues for the maintenance mode page

--- www/en/libs/handlers/system_page_maintenance.php
+++ www/en/libs/handlers/system_page_maintenance.php
@@ -2,7 +2,13 @@
 try{
     error_log('['.PROJECT.']['.(SUBENVIRONMENT ? SUBENVIRONMENT : 'NOSUBENVIRONMENT').'][MAINTENANCE] '.$reason);
     log_database('['.PROJECT.']['.(SUBENVIRONMENT ? SUBENVIRONMENT : 'NOSUBENVIRONMENT').'][MAINTENANCE] '.$reason, 'MAINTENANCE');
-    page_show('maintenance', true, $force, $data);
+
+    if($GLOBALS['page_is_admin']){
+        page_show('admin/maintenance', true, $force, $data);
+
+    }else{
+        page_show('maintenance', true, $force, $data);
+    }
 
 }catch(Exception $e){
     throw new bException('page_maintenance(): Failed', $e);
