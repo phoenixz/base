37647623f1e87e1a670007376b5e1fcd0e933813
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-03-01 23:18:14 -0600

    Added timer library that can store process timing

---
+++ init/framework/0.20.0.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Add timer library tables
+ */
+sql_query('DROP TABLE IF EXISTS `timer`');
+
+sql_query('CREATE TABLE `timers` (`id`            INT(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                  `createdon`     TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                  `createdby`     INT(11)       NOT NULL,
+                                  `start`         DATETIME          NULL,
+                                  `stop`          DATETIME          NULL,
+                                  `time`          INT(11)           NULL,
+                                  `process`       VARCHAR(32)       NULL,
+
+                                  INDEX (`createdon`),
+                                  INDEX (`createdby`),
+                                  INDEX (`start`),
+                                  INDEX (`time`),
+                                  INDEX (`process`)
+
+                                 ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['charset'].'" COLLATE="'.$_CONFIG['db']['collate'].'";');
+?>
---
+++ www/en/libs/timer.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Timer library
+ *
+ * This is an empty template library file
+ *
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Sven Oostenbrink <support@svenoostenbrink.com>
+ */
+
+
+
+/*
+ * Register timer in the database
+ */
+function timer_start($process){
+    try{
+        sql_query('INSERT INTO `timers` (`createdby`, `process`, `start`)
+                   VALUES               (:createdby , :process , NOW())',
+
+                   array(':createdby' => isset_get($_SESSION['user']['id']),
+                         ':process'   => $process));
+
+        $id = sql_insert_id();
+
+        $GLOBALS['timers'][$id] = microtime(true);
+
+        return $id;
+
+    }catch(Exception $e){
+        throw new bException('timer_start(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Update existing timer in database with stop time
+ */
+function timer_stop($id){
+    try{
+        if(empty($GLOBALS['timers'][$id])){
+            throw new bException(tr('timer_stop(): Specified timers id %id%" is not registered as a timer', array('%id%' => $id)), 'notexist');
+        }
+
+        $time = (integer) round((microtime(true) - $GLOBALS['timers'][$id]) * 1000, 0);
+
+        $r    = sql_query('UPDATE `timers`
+
+                           SET `stop`   = NOW(),
+                               `time`   = :time
+
+                           WHERE `id` = :id',
+
+                           array(':id'   => $id,
+                                 ':time' => $time));
+
+        if(!$r->numRows()){
+            throw new bException(tr('timer_stop(): Specified id %id%" exist in memory, but not in the database', array('%id%' => $id)), 'notexist');
+        }
+
+        unset($GLOBALS['timers'][$id]);
+
+        return $time;
+
+    }catch(Exception $e){
+        throw new bException('timer_stop(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Return timer information for the specified process
+ */
+function timer_get($process, $type = 'average'){
+    try{
+        if($time = sql_get('SELECT AVG(`time`) AS `time` FROM `timers` WHERE `process` = :process', 'time', array(':process' => $process))){
+            return $time;
+        }
+
+        throw new bException('timer_get(): Specified process "%process%" was not found', array('%process%' => $process), 'notfound');
+
+    }catch(Exception $e){
+        throw new bException('timer_get(): Failed', $e);
+    }
+}
+?>
