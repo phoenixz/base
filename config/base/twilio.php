9c5da2c207ce43602aad31a6811a7affa3a81c6d
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-03-19 18:14:22 -0600

    Added new configuration option $_CONFIG[twilio][display][auto_reload] that sets the automatic messages table update timeout

--- config/base/twilio.php
+++ config/base/twilio.php
@@ -7,5 +7,7 @@ $_CONFIG['twilio'] = array('accounts_id'    => null,
                            'conversations'  => array('size'             => 3,
                                                      'message_dates'    => false),
 
+                           'display'        => array('auto_reload'      => 60),
+
                            'defaults'       => array('country_code'     => 1));
 ?>
