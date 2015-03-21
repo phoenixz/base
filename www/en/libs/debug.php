8f3ab72591f56e08b273844801f9284f25a0836f
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-03-20 19:06:48 -0600

    Added die_in() to the debug library which in some cases is very nice to have to debug loops

--- www/en/libs/debug.php
+++ www/en/libs/debug.php
@@ -472,4 +472,21 @@ function debug_trace($filters = 'args'){
         throw new bException('debug_trace(): Failed', $e);
     }
 }
+
+
+
+/*
+ *
+ */
+function die_in($count, $message = null){
+    static $counter = 1;
+
+    if(!$message){
+        $message = tr('Terminated process because die counter reached "%count%"');
+    }
+
+    if($counter++ >= $count){
+        die(str_ends(str_replace('%count%', $count, $message), "\n"));
+    }
+}
 ?>
