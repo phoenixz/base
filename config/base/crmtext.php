5e8e8c51161ed77f0e84e3ff9295422d0ef2de78
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-04-02 18:53:01 -0600

    Added missing crmtext configuration

--- config/base/crmtext.php
+++ config/base/crmtext.php
@@ -1,3 +1,4 @@
 <?php
-$_CONFIG['crmtext'] = array('central_url' => 'https://restapi.crmtext.com/smapi/rest');
+$_CONFIG['crmtext'] = array('central_url'  => 'https://restapi.crmtext.com/smapi/rest',
+                            'callback_url' => domain('/sms/crm.php'));
 ?>
