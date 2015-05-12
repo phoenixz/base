aab33b4bf7f1b91545d22a252dbf1113ea1aa60d
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-05-11 20:29:01 -0500

    Adding cloudflare API library

---
+++ www/en/libs/cloudflare.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Cloud Flare library
+ *
+ * This library contains all functions to communicate easily with the Cloud Flare service
+ *
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Sven Oostenbrink <support@svenoostenbrink.com>
+ */
+
+
+
+/*
+ * Remove all files
+ */
+function cf_remove_file(){
+    try{
+
+    }catch(Exception $e){
+        throw new bException('cf_remove_file(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Remove all files
+ */
+function cf_remove_all(){
+    try{
+
+    }catch(Exception $e){
+        throw new bException('cf_remove_all(): Failed', $e);
+    }
+}
+?>
