106c3a87f0efc9cc91cae9203289a92a8318f84f
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-07-07 22:50:09 -0500

    Fixed various minor issues

--- www/en/admin/password.php
+++ www/en/admin/password.php
@@ -23,25 +23,23 @@ try{
                                    'password2' => isset_get($_POST['password']),
                                    'cpassword' => isset_get($_POST['cpassword'])));
 
-        html_flash_set(log_database('Updated password for user "'.str_log(user_name($_SESSION['user'])).'"', 'user_update'), 'success');
+        html_flash_set(log_database('Updated password for user "'.str_log(user_name($_SESSION['user'])).'"', 'update-password'), 'success');
         redirect(domain('/admin/profile.php'));
-    }
-    
-    /*
-     * Update pin
-     */
-    if(isset_get($_POST['doupdate2'])){
+
+    }elseif(isset_get($_POST['doupdate2'])){
         /*
+         * Update pin
          * Validate data
          */
         s_validate_pin($user);
 
-        /*user_update_pin(array('id'        => $_SESSION['user']['id'],
-                                   'pin'       => isset_get($_POST['pin']),
-                                   'pin2'      => isset_get($_POST['pin']),
-                                   'cpassword' => isset_get($_POST['cpassword2'])));
-        */
-        html_flash_set(log_database('Updated pin for user "'.str_log(user_name($_SESSION['user'])).'"', 'user_update'), 'success');
+// :TODO: Implement PIN support
+        //user_update_pin(array('id'        => $_SESSION['user']['id'],
+        //                      'pin'       => isset_get($_POST['pin']),
+        //                      'pin2'      => isset_get($_POST['pin']),
+        //                      'cpassword' => isset_get($_POST['cpassword'])));
+
+        html_flash_set(log_database(tr('Updated pin for user "%user"', array('%user%' => str_log(user_name($_SESSION['user'])))), 'update-pin'), 'success');
         redirect(domain('/admin/profile.php'));
     }
 
@@ -100,7 +98,7 @@ $html = '   <form name="user" id="user" action="'.domain(true).'" method="post">
                     </div>
                 </div>
             </form>
-            
+
             <form name="user2" id="user2" action="'.domain(true).'" method="post">
                 '.html_form().'
                 '.html_hidden($user).'
@@ -113,22 +111,22 @@ $html = '   <form name="user" id="user" action="'.domain(true).'" method="post">
                             </header>
                             <div class="panel-body">
                                 <div class="form-group">
-                                    <label class="col-md-3 control-label" for="cpassword2">'.tr('Current password').'</label>
+                                    <label class="col-md-3 control-label" for="cpassword">'.tr('Current password').'</label>
                                     <div class="col-md-6">
-                                        <input type="password" name="cpassword2" id="cpassword2" class="form-control" value="" maxlength="32">
+                                        <input type="password" name="cpassword" id="cpassword" class="form-control" value="" maxlength="32">
                                     </div>
                                 </div>
                                 <hr>
                                 <div class="form-group">
                                     <label class="col-md-3 control-label" for="pin">'.tr('Pin').'</label>
                                     <div class="col-md-6">
-                                        <input type="number" name="pin" id="pin" class="form-control" value="" length="4">
+                                        <input type="number" name="pin" id="pin" class="form-control" value="" length="6">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="col-md-3 control-label" for="pin2">'.tr('Validate pin').'</label>
                                     <div class="col-md-6">
-                                        <input type="number" name="pin2" id="pin2" class="form-control" value="" slength="4">
+                                        <input type="number" name="pin2" id="pin2" class="form-control" value="" slength="6">
                                     </div>
                                 </div>
                                 <input type="submit" class="mb-xs mt-xs mr-xs btn btn-primary" name="doupdate2" id="doupdate2" value="'.tr('Update').'">
@@ -149,14 +147,12 @@ $vj->validate('password' , 'required' , 'true'     , '<span class="FcbErrorTail"
 $vj->validate('password' , 'minlength', '8'        , '<span class="FcbErrorTail"></span>'.tr('Please ensure that the password has at least 8 characters'));
 $vj->validate('password2', 'equalTo'  , '#password', '<span class="FcbErrorTail"></span>'.tr('The password fields need to be equal'));
 
-$vj->validate('cpassword2', 'required' , 'true' , '<span class="FcbErrorTail"></span>'.tr('Please provide your current password'));
-$vj->validate('cpassword2', 'minlength', '8'    , '<span class="FcbErrorTail"></span>'.tr('Please ensure that your current password has at least 8 characters'));
-$vj->validate('pin'       , 'required' , 'true' , '<span class="FcbErrorTail"></span>'.tr('Please provide the pin code'));
-$vj->validate('pin'       , 'minlength', '4'    , '<span class="FcbErrorTail"></span>'.tr('Please ensure that the pin code has 4 digits'));
-$vj->validate('pin'       , 'maxlength', '4'    , '<span class="FcbErrorTail"></span>'.tr('Please ensure that the pin code has 4 digits'));
-$vj->validate('pin2'      , 'equalTo'  , '#pin' , '<span class="FcbErrorTail"></span>'.tr('The password fields need to be equal'));
-
-
+$vj->validate('cpassword', 'required' , 'true'     , '<span class="FcbErrorTail"></span>'.tr('Please provide your current password'));
+$vj->validate('cpassword', 'minlength', '8'        , '<span class="FcbErrorTail"></span>'.tr('Please ensure that your current password has at least 8 characters'));
+$vj->validate('pin'      , 'required' , 'true'     , '<span class="FcbErrorTail"></span>'.tr('Please provide the pin code'));
+$vj->validate('pin'      , 'minlength', '4'        , '<span class="FcbErrorTail"></span>'.tr('Please ensure that the pin code has at least digits'));
+$vj->validate('pin'      , 'maxlength', '6'        , '<span class="FcbErrorTail"></span>'.tr('Please ensure that the pin code has at most 6 digits'));
+$vj->validate('pin2'     , 'equalTo'  , '#pin'     , '<span class="FcbErrorTail"></span>'.tr('The pin fields need to be equal'));
 
 $html .= $vj->output_validation(array('id'   => 'user',
                                       'json' => false));
@@ -178,9 +174,9 @@ function s_validate_password(&$user, $id = null){
     try{
         // Validate input
         $v = new validate_form($user, 'password,password2,cpassword');
-        
-        $v->hasMinChars ($user['cpassword'],                  8, tr('Please ensure that your current password has a minimum of 8 characters'));
-        $v->hasMinChars ($user['password'] ,                  8, tr('Please ensure that the password has a minimum of 8 characters'));
+
+        $v->hasMinChars ($user['cpassword'],                  8, tr('Please ensure that your current password has at least 8 characters'));
+        $v->hasMinChars ($user['password'] ,                  8, tr('Please ensure that the password has at least 8 characters'));
         $v->isEqual     ($user['password'] , $user['password2'], tr('Please ensure that the password and validation password match'));
 
     }catch(Exception $e){
@@ -188,18 +184,23 @@ function s_validate_password(&$user, $id = null){
     }
 }
 
+
+/*
+ *
+ */
 function s_validate_pin(&$user, $id = null){
     try{
         // Validate input
-        $v = new validate_form($user, 'pin,pin2,cpassword2');
-    
-        $v->hasMinChars ($user['cpassword2'],             8, tr('Please ensure that your current password has a minimum of 8 characters'));
-        $v->hasMinChars ($user['pin']       ,             4, tr('Please ensure that the pin has 4 digits'));
-        $v->hasMaxChars ($user['pin']       ,             4, tr('Please ensure that the pin has 4 digits'));
+        $v = new validate_form($user, 'pin,pin2,cpassword');
+
+        $v->hasMinChars ($user['cpassword'],             8, tr('Please ensure that your current password has a minimum of 8 characters'));
+        $v->hasMinChars ($user['pin']       ,             4, tr('Please ensure that the pin has at least 4 digits'));
+        $v->hasMaxChars ($user['pin']       ,             6, tr('Please ensure that the pin has at most 6 digits'));
         $v->isEqual     ($user['pin']       , $user['pin2'], tr('Please ensure that the pin and validation pin match'));
+        $v->isNumeric   ($user['pin']                      , tr('Please ensure that the pin is numeric'));
 
     }catch(Exception $e){
-        throw new bException('s_validate_password(): Failed', $e);
+        throw new bException('s_validate_pin(): Failed', $e);
     }
 }
 ?>
