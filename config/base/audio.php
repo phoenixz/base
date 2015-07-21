397da91996c2b5144667af8bbe93e6a655877eb0
Ismael Haro <isma@briskadvertising.com>
2015-07-21 17:35:42 -0500

    Added audio library and default audio configurations

---
+++ config/base/audio.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Audio configuration, there are classes of audio that can be played
+ * and also there are different commands to play different types of audio.
+ */
+$_CONFIG['audio'] = array('classes' => array('alarm'     => 'alarm.mp3',
+                                             'exception' => 'critical.mp3',
+                                             'notify'    => 'ping.mp3',
+                                             'default'   => 'alarm.mp3'),
+                          'command' => 'mplayer',
+                          'default' => 'exception');
+
+?>
