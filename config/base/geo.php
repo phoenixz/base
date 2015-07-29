3082a1c31475d61c109b46220cce62feef86949c
Sven Oostenbrink <so.oostenbrink@gmail.com>
2015-07-29 14:14:03 -0500

    Added AJAX location setter, added configurable location lookup

--- config/base/geo.php
+++ config/base/geo.php
@@ -9,7 +9,8 @@
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Sven Oostenbrink <support@svenoostenbrink.com>, Johan Geuze
  */
-$_CONFIG['geo'] = array('cities' => array('filter_type' => ' OR ',
+$_CONFIG['geo'] = array('lookup' => 'geonames',
+                        'cities' => array('filter_type' => ' OR ',
                                           'filters'     => array('min_population' => 200000,
                                                                  'feature_code'   => 'PPLA,PPLA2')));
 ?>
