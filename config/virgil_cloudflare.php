2c0d029c6339db36c3adc5372e1b9cba0d27d0e5
Virgilio Vazquez <vrgilius@gmail.com>
2015-10-02 16:06:49 -0500

    Virgil Cloudflare configuration

---
+++ config/virgil_cloudflare.php
@@ -0,0 +1,7 @@
+<?php
+
+$_CONFIG['cloudflare'] = array('email'   => 'vrgilius@gmail.com',
+                               'API-key' => 'f2a1a19acb45878003299a43a84c018a80c6e'
+                              )
+
+?>
