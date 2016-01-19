fca9d8846653946d37a10acac1cff66389a028d6
Virgilio Vazquez <vrgilius@gmail.com>
2016-01-18 19:26:50 -0600

    Added <pre> tag to error messages to display them correctly on email

--- www/en/libs/handlers/system_php_error_handler.php
+++ www/en/libs/handlers/system_php_error_handler.php
@@ -2,7 +2,7 @@
 $session = "\n\nSESSION DATA\n".print_r(isset_get($_SESSION), true);
 $server  = "\n\nSERVER DATA\n".print_r(isset_get($_SERVER), true);
 
-notify('error', 'PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session);
+notify('error', '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session.'</pre>');
 
 if(PLATFORM == 'http'){
     error_log('PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"');
