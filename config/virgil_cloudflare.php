ec105760ea279c2622e85d5f15d2a75f73bb2d46
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-01-26 12:11:57 -0600

    Removed user specific configuration

--- config/virgil_cloudflare.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$_CONFIG['cloudflare'] = array('email'   => 'vrgilius@gmail.com',
-                               'API-key' => 'f2a1a19acb45878003299a43a84c018a80c6e'
-                              )
-
-?>
