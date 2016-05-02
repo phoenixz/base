2e8e131daaeac76fac492919c8e961ee92d96abd
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-05-01 21:55:03 -0500

    Added name() to system library
    This will replace user_name() soon

--- www/en/libs/system.php
+++ www/en/libs/system.php
@@ -1779,4 +1779,50 @@ function api_prefix($id = null, $force_environment = false){
         throw new bException(tr('api_prefix(): Failed'), $e);
     }
 }
+
+
+
+/*
+ *
+ */
+function name($user = null, $key_prefix = ''){
+    try{
+        if($user){
+            if(is_scalar($user)){
+                if(!is_numeric($user)){
+                    /*
+                     * String, assume its a username
+                     */
+                    return $user;
+                }
+
+                /*
+                 * This is not a user assoc array, but a user ID.
+                 * Fetch user data from DB, then treat it as an array
+                 */
+                if(!$user = sql_get('SELECT `name` `username`, `email` FROM `users` WHERE `id` = :id', array(':id' => $user))){
+                   throw new bException('name(): Specified user id ":id" does not exist', array(':id' => str_log($user)), 'notexist');
+                }
+            }
+
+            if(!is_array($user)){
+                throw new bException(tr('name(): Invalid data specified, please specify either user id, name, or an array containing username, email and or id'), 'invalid');
+            }
+
+            $user = not_empty(isset_get($user[$key_prefix.'name']), isset_get($user[$key_prefix.'username']), isset_get($user[$key_prefix.'email']), isset_get($user[$key_prefix.'id']));
+
+            if($user){
+                return $user;
+            }
+        }
+
+        /*
+         * No user data found, assume guest user.
+         */
+        return tr('Guest');
+
+    }catch(Exception $e){
+        throw new bException(tr('name(): Failed'), $e);
+    }
+}
 ?>
