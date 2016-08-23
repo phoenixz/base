791d768f47cdd31ecc00a756089d2212d245e9da
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-08-23 18:17:12 -0500

    Added library whois
    The whois library will contain functions to execute whois functionalities

---
+++ www/en/libs/whois.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Whois library
+ *
+ * This library contains functions to execute whois functionalities
+ *
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Sven Oostenbrink <support@svenoostenbrink.com>
+ */
+
+
+/*
+ * Empty function
+ */
+function whois($domain){
+    try{
+
+    }catch(Exception $e){
+        throw new bException('whois(): Failed', $e);
+    }
+}
+?>
