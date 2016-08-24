5ffc433bad846e2c12ce357374652fdf6b0063a3
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-08-23 19:53:36 -0500

    Added required configuration file for json library

--- config/base/json.php
+++ config/base/json.php
@@ -5,5 +5,5 @@
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Sven Oostenbrink <support@svenoostenbrink.com>,
  */
-$_CONFIG['webdom']['auth_key'] = '787c0151cc8b63b65565de743456d1fa3e0c90a09ec963a6f8f0fa6bf653d461';
+$_CONFIG['json'] = array('auth_key' => '');
 ?>
