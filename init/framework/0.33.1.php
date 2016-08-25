99a36ca99674be932df40cb2160bbee34477d8bf
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-08-25 00:48:21 -0500

    Fixed init file SQL query

--- init/framework/0.33.1.php
+++ init/framework/0.33.1.php
@@ -2,5 +2,5 @@
 /*
  * Add white label domain support
  */
-sql_column_exists('users', 'domain', '!ALTER TABLE `users` ADD COLUMN `domain` VARCHAR(128) NULL AFER `password`');
+sql_column_exists('users', 'domain', '!ALTER TABLE `users` ADD COLUMN `domain` VARCHAR(128) NULL AFTER `password`');
 ?>
