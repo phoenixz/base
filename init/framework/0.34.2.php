5cbf62ec931158d72fcce7469e609e9ae5732c42
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-09-08 05:13:30 -0500

    FV0.34.2: Cleaned up blogs table

---
+++ init/framework/0.34.2.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Fix blog table, remove old garbage
+ */
+sql_index_exists ('blogs', 'language'   , '!ALTER TABLE `blogs` DROP INDEX  `language`');
+sql_index_exists ('blogs', 'rights_id'  , '!ALTER TABLE `blogs` DROP INDEX  `rights_id`');
+sql_index_exists ('blogs', 'seokeywords', '!ALTER TABLE `blogs` DROP INDEX  `seokeywords`');
+
+sql_column_exists('blogs', 'language'   , '!ALTER TABLE `blogs` DROP COLUMN `language`');
+sql_column_exists('blogs', 'rights_id'  , '!ALTER TABLE `blogs` DROP COLUMN `rights_id`');
+sql_column_exists('blogs', 'seokeywords', '!ALTER TABLE `blogs` DROP COLUMN `seokeywords`');
+?>
