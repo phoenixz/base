c9ea0431a3cedeb705f9067426b7de2ccf3a54ed
Juan Jose Muñoz Ortega <john.milo2884@gmail.com>
2016-10-15 23:41:46 -0500

    Add to function to read arrays in javascript

--- www/en/libs/html.php
+++ www/en/libs/html.php
@@ -138,7 +138,7 @@ function html_generate_css(){
         foreach($GLOBALS['css'] as $file => $meta) {
             if(!$file) continue;
 
-            $html = '<link rel="stylesheet" type="text/css" href="'.cdn_prefix((($_CONFIG['whitelabels']['enabled'] === true) ? $_SESSION['domain']. '/' : '').'css/'.(!empty($GLOBALS['page_is_mobile']) ? 'mobile/' : '').$file.($min ? '.min.css' : '.css')).'"'.($meta['media'] ? ' media="'.$meta['media'].'"' : '').'>';
+            $html = '<link rel="stylesheet" type="text/css" href="'.cdn_prefix((($_CONFIG['whitelabels']['enabled'] === true) ? $_SESSION['domain']. '/' : '').($GLOBALS['page_is_admin'] ? 'admin/' : '').'css/'.(!empty($GLOBALS['page_is_mobile']) ? 'mobile/' : '').$file.($min ? '.min.css' : '.css')).'"'.($meta['media'] ? ' media="'.$meta['media'].'"' : '').'>';
 
             if(substr($file, 0, 2) == 'ie'){
                 $retval .= html_iefilter($html, str_until(str_from($file, 'ie'), '.'));
@@ -322,7 +322,7 @@ function html_generate_js(){
                     if($skip) continue;
                 }
 
-                $html = '<script'.(!empty($data['option']) ? ' '.$data['option'] : '').' type="text/javascript" src="'.cdn_prefix((($_CONFIG['whitelabels']['enabled'] === true) ? $_SESSION['domain'].'/' : '').'js/'.$file.$min.'.js').'"></script>';
+                $html = '<script'.(!empty($data['option']) ? ' '.$data['option'] : '').' type="text/javascript" src="'.cdn_prefix((($_CONFIG['whitelabels']['enabled'] === true) ? $_SESSION['domain'].'/' : '').($GLOBALS['page_is_admin'] ? 'admin/' : '').'js/'.$file.$min.'.js').'"></script>';
             }
 
             /*
@@ -1019,6 +1019,9 @@ function html_select($params){
         /*
          * Autosubmit on the specified selector
          */
+        $params['autosubmit'] = str_replace('[', '\\\\[', $params['autosubmit']);
+        $params['autosubmit'] = str_replace(']', '\\\\]', $params['autosubmit']);
+
         return $retval.html_script('$("'.$params['autosubmit'].'").change(function(){ $(this).closest("form").find("input,textarea,select").addClass("ignore"); $(this).closest("form").submit(); });');
 
     }catch(Exception $e){
