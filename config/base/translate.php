323441e0e033dde0f5f909316d2d97a1f9eed999
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-10-20 01:21:55 -0500

    Fixed translator confguration

--- config/base/translate.php
+++ config/base/translate.php
@@ -1,6 +1,18 @@
 <?php
-
-$_CONFIG['translate'] = array(// Maximum time difference in seconds between server and client
-                                'max_difference_time' => 3 );
+/*
+ * Translation system configuration
+ *
+ * mode defines what to do when no translations are found
+ * possible values : strict               (all strings must be translated, fails if thats not possible)
+                     full                 (search for alternative translations on other projects
+                                           if they are not available on the current one (fails if not found))
+                     most                 (same as above but doesnt fail if not translations are found)
+                     none                 (leave original strings if not translations are found)
+ */
+$_CONFIG['translate'] = array('url'          => 'http://toolkit.ingiga.com/api/translate.php',
+                              'mode'         => 'full',
+                              'passphrase'   => 'translate',
+                              'api_key'      => '',
+                              'allowed_tags' => '<b><a><strong>');
 
 ?>
