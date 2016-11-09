1a695fdbbd2ea5a3910ddbc52de32bb99962fb5e
Juan Jose Muñoz Ortega <john.milo2884@gmail.com>
2016-11-08 20:02:23 -0600

    Fixed FV0.37.0 init file
    Fixed table design issues, added auto twilio config to table copy

--- init/framework/0.37.0.php
+++ init/framework/0.37.0.php
@@ -8,15 +8,15 @@ sql_query('DROP TABLE IF EXISTS `twilio_accounts`');
 
 
 
-sql_query('CREATE TABLE `twilio_accounts` (`id`              INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                           `createdon`       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                           `createdby`       INT(11)          NULL,
-                                           `modifiedon`      DATETIME         NULL,
-                                           `modifiedby`      INT(11)          NULL,
-                                           `status`          VARCHAR(16)      NULL,
-                                           `email`           VARCHAR(128)     NULL,
-                                           `accounts_id`     VARCHAR(40)      NULL,
-                                           `accounts_tokens` VARCHAR(40)      NULL,
+sql_query('CREATE TABLE `twilio_accounts` (`id`             INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                           `createdon`      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                           `createdby`      INT(11)          NULL,
+                                           `modifiedon`     DATETIME         NULL,
+                                           `modifiedby`     INT(11)          NULL,
+                                           `status`         VARCHAR(16)      NULL,
+                                           `email`          VARCHAR(128)     NULL,
+                                           `accounts_id`    VARCHAR(40)      NULL,
+                                           `accounts_token` VARCHAR(40)      NULL,
 
                                            INDEX (`createdon`),
                                            INDEX (`createdby`),
@@ -77,4 +77,31 @@ sql_query('CREATE TABLE `twilio_numbers` (`id`          INT(11)      NOT NULL AU
                                           CONSTRAINT `fk_twilio_numbers_groups_id`   FOREIGN KEY (`groups_id`)   REFERENCES `twilio_groups`   (`id`) ON DELETE RESTRICT
 
                                          ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
+
+load_config('twilio');
+cli_log(tr('Copying twilio configuration...'), 'white');
+
+$r_account = sql_prepare('INSERT INTO `twilio_accounts` (`email`, `accounts_id`, `accounts_token`)
+                          VALUES                        (:email , :accounts_id , :accounts_token )');
+
+$r_number = sql_prepare('INSERT INTO `twilio_numbers` (`accounts_id`, `number`, `name`)
+                         VALUES                       (:accounts_id , :number , :name )');
+
+foreach($_CONFIG['twilio']['accounts'] as $email => $data){
+    cli_dot(1);
+    $r_account->execute(array(':email'          => $email,
+                              ':accounts_id'    => $data['accounts_id'],
+                              ':accounts_token' => $data['accounts_token']));
+
+    $accounts_id = sql_insert_id();
+
+    foreach($data['sources'] as $number => $name){
+        cli_dot(1);
+        $r_number->execute(array(':accounts_id' => $accounts_id,
+                                 ':number'      => $number,
+                                 ':name'        => $name));
+    }
+}
+
+cli_dot(false);
 ?>
