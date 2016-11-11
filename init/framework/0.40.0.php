4ad62cedc28cf3bc45208e7b77c3d239e29fdd42
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-11-10 20:33:48 -0600

    Fixed init SQL

--- init/framework/0.40.0.php
+++ init/framework/0.40.0.php
@@ -4,29 +4,29 @@
  */
 sql_query('DROP TABLE IF EXISTS `whitelabel`;');
 
-sql_query('CREATE TABLE `whitelabel` (`id`          INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                      `createdon`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                      `createdby`   INT(11)          NULL,
-                                      `modifiedon`  DATETIME         NULL,
-                                      `modifiedby`  INT(11)          NULL,
-                                      `status`      VARCHAR(16)      NULL,
-                                      `users_id`    INT(11)      NOT NULL,
-                                      `name`        VARCHAR(64)  NOT NULL,
-                                      `seoname`     VARCHAR(64)  NOT NULL,
-                                      `domain`      VARCHAR(128) NOT NULL,
+sql_query('CREATE TABLE `whitelabels` (`id`          INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                       `createdon`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                       `createdby`   INT(11)          NULL,
+                                       `modifiedon`  DATETIME         NULL,
+                                       `modifiedby`  INT(11)          NULL,
+                                       `status`      VARCHAR(16)      NULL,
+                                       `users_id`    INT(11)      NOT NULL,
+                                       `name`        VARCHAR(64)  NOT NULL,
+                                       `seoname`     VARCHAR(64)  NOT NULL,
+                                       `domain`      VARCHAR(128) NOT NULL,
 
-                                      INDEX (`createdon`),
-                                      INDEX (`createdby`),
-                                      INDEX (`modifiedon`),
-                                      INDEX (`modifiedby`),
-                                      INDEX (`status`),
-                                      UNIQUE(`seoname`),
-                                      UNIQUE(`domain`),
-                                      UNIQUE(`users_id`),
+                                       INDEX (`createdon`),
+                                       INDEX (`createdby`),
+                                       INDEX (`modifiedon`),
+                                       INDEX (`modifiedby`),
+                                       INDEX (`status`),
+                                       UNIQUE(`seoname`),
+                                       UNIQUE(`domain`),
+                                       UNIQUE(`users_id`),
 
-                                      CONSTRAINT `fk_whitelabel_createdby`  FOREIGN KEY (`createdby`)  REFERENCES `users` (`id`) ON DELETE RESTRICT,
-                                      CONSTRAINT `fk_whitelabel_modifiedby` FOREIGN KEY (`modifiedby`) REFERENCES `users` (`id`) ON DELETE RESTRICT,
-                                      CONSTRAINT `fk_whitelabel_users_id`   FOREIGN KEY (`users_id`)   REFERENCES `users` (`id`) ON DELETE RESTRICT
+                                       CONSTRAINT `fk_whitelabels_createdby`  FOREIGN KEY (`createdby`)  REFERENCES `users` (`id`) ON DELETE RESTRICT,
+                                       CONSTRAINT `fk_whitelabels_modifiedby` FOREIGN KEY (`modifiedby`) REFERENCES `users` (`id`) ON DELETE RESTRICT,
+                                       CONSTRAINT `fk_whitelabels_users_id`   FOREIGN KEY (`users_id`)   REFERENCES `users` (`id`) ON DELETE RESTRICT
 
                                       ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
 ?>
