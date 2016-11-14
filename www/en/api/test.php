e59b1055c10d0e05297c15c27f0d31c48e9b7a15
Sven Oostenbrink <so.oostenbrink@gmail.com>
2016-11-14 14:24:50 -0600

    Implemented test API file

---
+++ www/en/api/test.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Webdom API
+ */
+require_once(__DIR__.'/../libs/startup.php');
+load_libs('json');
+
+
+
+/*
+ * Process requests
+ */
+try{
+    switch(isset_get($_GET['method'])){
+        case 'auth':
+            json_reply(array('token' => json_authenticate(isset_get($_GET['key']))));
+            break;
+
+        case 'close':
+            json_stop_session(isset_get($_GET['token']));
+            json_reply();
+            break;
+
+        case 'get_message':
+            json_start_session(isset_get($_GET['token']));
+            json_reply(array('email'   => 'info@ingiga.com',
+                             'message' => 'This is just a test reply'));
+            break;
+
+        case '':
+            throw new bException(tr('No "method" specified'), 'not-specifed');
+
+        default:
+            throw new bException(tr('Unknown method ":method" specified', array(':method' => $_GET['method'])), 'not-specifed');
+    }
+
+}catch(Exception $e){
+    json_error($e);
+}
+?>
