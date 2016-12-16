f8b6bf919d69a82e5d980fce2177f576807f786e
Cristobal Castillo <cristobal@ingiga.com>
2016-12-15 20:31:11 -0600

    implementation ssh control for cdn system

--- www/en/libs/ssh.php
+++ www/en/libs/ssh.php
@@ -90,4 +90,172 @@ function ssh_get_account($account){
         throw new bException('ssh_get_account(): Failed', $e);
     }
 }
+
+
+
+/*
+ *
+ */
+function ssh_start_control_master($server, $socket = null){
+    global $_CONFIG;
+
+    try{
+        load_libs('file');
+        file_ensure_path(TMP);
+
+        if(!$socket){
+            $socket = file_temp();
+        }
+
+        if(ssh_get_control_master($socket)){
+            return $socket;
+        }
+
+        $result = ssh_exec(array('hostname'  => $server['domain'],
+                                 'port'      => $_CONFIG['cdn']['port'],
+                                 'username'  => $server['username'],
+                                 'ssh_key'   => ssh_get_key($server['username']),
+                                 'arguments' => '-nNf -o ControlMaster=yes -o ControlPath='.$socket), ' 2>&1 >'.ROOT.'/data/log/ssh_master');
+
+        return $socket;
+
+    }catch(Exception $e){
+showdie($e);
+        throw new bException('ssh_start_control_master(): Failed', $e);
+    }
+}
+
+
+
+/*
+ *
+ */
+function ssh_get_control_master($socket = null){
+    global $_CONFIG;
+
+    try{
+        $result = safe_exec('ps $(pgrep --full '.$socket.') | grep "ssh -nNf" | grep --invert-match pgrep', '0,1');
+        $result = array_pop($result);
+
+        preg_match_all('/^\s*\d+/', $result, $matches);
+
+        $pid = array_pop($matches);
+        $pid = (integer) array_pop($pid);
+
+        return $pid;
+
+    }catch(Exception $e){
+showdie($e);
+        throw new bException('ssh_get_control_master(): Failed', $e);
+    }
+}
+
+
+
+/*
+ *
+ */
+function ssh_stop_control_master($socket = null){
+    global $_CONFIG;
+
+    try{
+        $pid = ssh_get_control_master($socket);
+
+        if(!posix_kill($pid, 15)){
+            return posix_kill($pid, 9);
+        }
+
+        return true;
+
+    }catch(Exception $e){
+showdie($e);
+        throw new bException('ssh_stop_control_master(): Failed', $e);
+    }
+}
+
+
+
+/*
+ *
+ */
+function ssh_exec($server, $commands = null){
+
+    try{
+
+        array_params($server);
+        array_default($server, 'hostname'     , '');
+        array_default($server, 'ssh_key'      , '');
+        array_default($server, 'port'         , 22);
+        array_default($server, 'hostkey_check', false);
+        array_default($server, 'arguments'    , '-T');
+
+        if(!$server['hostkey_check']){
+            $server['arguments'] .= ' -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ';
+        }
+
+        /*
+         * Ensure that ssh_keys directory exists and that its safe
+         */
+        load_libs('file');
+        file_ensure_path(ROOT.'data/ssh_keys');
+        chmod(ROOT.'data/ssh_keys', 0770);
+
+        /*
+         * Safely create SSH key file
+         */
+        $keyfile = ROOT.'data/ssh_keys/'.str_random(8);
+
+        touch($keyfile);
+        chmod($keyfile, 0600);
+        file_put_contents($keyfile, $server['ssh_key'], FILE_APPEND);
+        chmod($keyfile, 0400);
+
+        /*
+         * Execute command on remote server
+         */
+//show('ssh '.$server['arguments'].' -p '.$server['port'].' -i '.$keyfile.' '.$server['username'].'@'.$server['hostname'].' '.$commands);
+        $result = safe_exec('ssh '.$server['arguments'].' -p '.$server['port'].' -i '.$keyfile.' '.$server['username'].'@'.$server['hostname'].' '.$commands);
+
+        chmod($keyfile, 0600);
+        file_delete($keyfile);
+
+        return $result;
+
+    }catch(Exception $e){
+showdie($e);
+        notify(tr('ssh_exec() exception'), $e, 'developers');
+
+        /*
+         * Try deleting the keyfile anyway!
+         */
+        try{
+            if(!empty($keyfile)){
+                safe_exec(chmod($keyfile, 0600));
+                file_delete($keyfile);
+            }
+
+        }catch(Exception $e){
+            /*
+             * Cannot be deleted, just ignore and notify
+             */
+            notify(tr('ssh_exec() cannot delete key'), $e, 'developers');
+        }
+
+        throw new bException('ssh_exec(): Failed', $e);
+    }
+}
+
+
+
+/*
+ *
+ */
+function ssh_get_key($username){
+    try{
+        return sql_get('SELECT `ssh_key` FROM `ssh_accounts` WHERE `username` = :username', 'ssh_key', array(':username' => $username));
+
+    }catch(Exception $e){
+        throw new bException('ssh_get_key(): Failed', $e);
+    }
+}
 ?>
