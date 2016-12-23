6424de9bc6cd67da214a3045a15774bafd42ab94
Yenuan Gordillo <yetto.carpediem@gmail.com>
2016-12-23 17:22:59 -0600

    Fixed last quirks in gmail.php

--- www/en/libs/gmail.php
+++ www/en/libs/gmail.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * GEOIP library
+ *
+ * This library contains functions to manage GEO IP detection
+ *
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Johan Geuze, Sven Oostenbrink <support@ingiga.com>
+ */
 
 
 
@@ -6,27 +14,18 @@
  * Expects $data array from email pool fuction
  */
 function get_gmail_vcode($data){
-  /*
-   * Attemps to find google verification codes
-   */
-  if (strpos($data['from'],'forwarding-noreply@google.com') !== false) {
-
-    $matches_code;
-    $matches_from;
-    preg_match_all("/: \d{9}/", $data['text'], $matches_code);
-    preg_match_all("/[A-Za-z0-9_%+-\.]{3,}@[\.a-z0-9-]{3,}\.[a-z]{2,}/", $data['subject'], $matches_from);
-
     /*
-     * Returns code and email address
+     * Attemps to find google verification codes
      */
-    $retval = array('code' => substr($matches_code[0][0], 2),
-               'from' => $matches_from[0][0]);
-    return $retval;
+    if (strpos($data['from'],'forwarding-noreply@google.com') !== false) {
+        preg_match_all('/: \d{9}/', $data['text'], $matches_code);
+        preg_match_all('/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/', $data['subject'], $matches_from);
 
-  }
-
-  return null;
-
+        /*
+        * Returns code and email address
+        */
+        return array('code' => substr(isset_get($matches_code[0][0]), 2),
+                     'from' => isset_get($matches_from[0][0]));
+    }
 }
-
 ?>
