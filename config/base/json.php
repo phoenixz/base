7ffa7766d8ca0c1b3a7b5eb4ee155ec5ad5c9344
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-01-07 21:40:53 -0600

    Added "obsolete_support" to json configuration

--- config/base/json.php
+++ config/base/json.php
@@ -5,5 +5,6 @@
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Sven Oostenbrink <support@ingiga.com>,
  */
-$_CONFIG['json'] = array('auth_key' => '');
+$_CONFIG['json']                                            = array('auth_key'         => '',
+                                                                    'obsolete_support' => true);    // json_reply() by default returns both "data" and "message" data payloads
 ?>
