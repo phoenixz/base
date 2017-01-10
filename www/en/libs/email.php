66e0cfaaeaa53b6c8e5b7e98cd6c83473f6a7560
Yenuan Gordillo <yetto.carpediem@gmail.com>
2017-01-10 15:04:14 -0600

    Fixed backwards logic on email forward option

--- www/en/libs/email.php
+++ www/en/libs/email.php
@@ -184,10 +184,10 @@ function email_poll($params){
                 if($userdata['email'] !== $data['to']){
                     switch($_CONFIG['email']['forward_option']){
                         case 'source':
-                            $data['to'] = $userdata['email'];
                             break;
 
                         case 'target':
+                            $data['to'] = $userdata['email'];
                             break;
 
                         case 'account':
@@ -196,10 +196,10 @@ function email_poll($params){
                              */
                             switch($userdata['forward_option']){
                                 case 'source':
-                                    $data['to'] = $userdata['email'];
                                     break;
 
                                 case 'target':
+                                    $data['to'] = $userdata['email'];
                                     break;
 
                                 default:
