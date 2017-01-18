4b3494ef37adf4c1f84b7557f2ea35822d9ecd58
Juan Jose Munoz Ortega <juan@ingiga.com>
2017-01-17 19:30:37 -0600

    fixed column and add more information refer to init

--- init/framework/0.44.1.php
+++ init/framework/0.44.1.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Add hash column to blogs_media
+ * Add hash to no save duplicated images in blogs_media
  */
 
-sql_column_exists('blogs_media', 'hash' , '!ALTER TABLE `blogs_media` ADD COLUMN `hash`  VARCHAR(40) NULL DEFAULT NULL AFTER `original`');
+sql_column_exists('blogs_media', 'hash', '!ALTER TABLE `blogs_media` ADD COLUMN `hash` CHAR(40) NULL DEFAULT NULL AFTER `original`');
 
 ?>
