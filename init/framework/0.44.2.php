94bb80d08761ec3f0be1f66276f84cd94ff599db
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-01-19 14:47:19 -0600

    Fixed init file issues
    Fixed sitemaps_data priority column, added hash column to blogs_media

--- init/framework/0.44.2.php
+++ init/framework/0.44.2.php
@@ -22,4 +22,9 @@ sql_column_exists('ads_images', 'clusters_id', '!ALTER TABLE `ads_images` ADD IN
 sql_foreignkey_exists('ads_images', 'fk_ads_images_clusters_id', '!ALTER TABLE `ads_images` ADD CONSTRAINT `fk_ads_images_clusters_id` FOREIGN KEY (`clusters_id`) REFERENCES `forwarder_clusters` (`id`) ON DELETE RESTRICT;');
 
 sql_query('ALTER TABLE `ads_campaigns` CHANGE COLUMN `seoname` `seoname` VARCHAR(64) NULL DEFAULT NULL');
+
+/*
+ * Add hash to no save duplicated images in blogs_media
+ */
+sql_column_exists('blogs_media', 'hash', '!ALTER TABLE `blogs_media` ADD COLUMN `hash` CHAR(40) NULL DEFAULT NULL AFTER `original`');
 ?>
