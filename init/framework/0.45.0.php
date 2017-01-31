1472ba9cf961b55b935ca316589e1ea33736c844
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-01-31 04:59:42 -0600

    Fixed init 0.45.0
    Added user authentication failure management columns to `users` table

--- init/framework/0.45.0.php
+++ init/framework/0.45.0.php
@@ -3,29 +3,34 @@
  * Add authentication registration support
  *
  * Add users messages support
+ *
+ * Add user authentication failure tracking and user account locking
  */
+sql_column_exists('users', 'auth_fails'  , '!ALTER TABLE `users` ADD COLUMN `auth_fails`   INT(11)  NOT NULL AFTER `last_signin`');
+sql_column_exists('users', 'locked_until', '!ALTER TABLE `users` ADD COLUMN `locked_until` DATETIME     NULL AFTER `auth_fail`');
+
 sql_query('DROP TABLE IF EXISTS `authentications`');
 sql_query('DROP TABLE IF EXISTS `users_messages`');
 
-sql_query('CREATE TABLE `users_messages` (`id`               INT(11)      NOT NULL AUTO_INCREMENT,
-                                          `createdon`        TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                          `createdby`        INT(11)      NOT NULL,
-                                          `modifiedon`       DATETIME         NULL DEFAULT NULL,
-                                          `modifiedby`       INT(11)          NULL DEFAULT NULL,
-                                          `status`           VARCHAR(16)      NULL DEFAULT NULL,
-                                          `users_id`         INT(11)      NOT NULL,
-                                          `priority`         ENUM("low", "medium", "high") NOT NULL,
-                                          `subject`          VARCHAR(255) NULL DEFAULT NULL,
-                                          `message`          TEXT         NULL DEFAULT NULL,
+sql_query('CREATE TABLE `users_messages` (`id`         INT(11)      NOT NULL AUTO_INCREMENT,
+                                          `createdon`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                          `createdby`  INT(11)      NOT NULL,
+                                          `modifiedon` DATETIME         NULL DEFAULT NULL,
+                                          `modifiedby` INT(11)          NULL DEFAULT NULL,
+                                          `status`     VARCHAR(16)      NULL DEFAULT NULL,
+                                          `users_id`   INT(11)      NOT NULL,
+                                          `priority`   ENUM("low", "medium", "high") NOT NULL,
+                                          `subject`    VARCHAR(255) NULL DEFAULT NULL,
+                                          `message`    TEXT         NULL DEFAULT NULL,
 
-                                          PRIMARY KEY `id`           (`id`),
-                                                  KEY `createdon`    (`createdon`),
-                                                  KEY `createdby`    (`createdby`),
-                                                  KEY `modifiedon`   (`modifiedon`),
-                                                  KEY `status`       (`status`),
-                                                  KEY `priority`     (`priority`),
-                                                  KEY `users_id`     (`users_id`),
-                                                  KEY `subject`      (`subject`),
+                                          PRIMARY KEY `id`         (`id`),
+                                                  KEY `createdon`  (`createdon`),
+                                                  KEY `createdby`  (`createdby`),
+                                                  KEY `modifiedon` (`modifiedon`),
+                                                  KEY `status`     (`status`),
+                                                  KEY `priority`   (`priority`),
+                                                  KEY `users_id`   (`users_id`),
+                                                  KEY `subject`    (`subject`),
 
                                           CONSTRAINT `fk_users_messages_createdby`  FOREIGN KEY (`createdby`)  REFERENCES `users` (`id`) ON DELETE RESTRICT,
                                           CONSTRAINT `fk_users_messages_modifiedby` FOREIGN KEY (`modifiedby`) REFERENCES `users` (`id`) ON DELETE RESTRICT,
@@ -33,14 +38,15 @@ sql_query('CREATE TABLE `users_messages` (`id`               INT(11)      NOT NU
 
                                          ) ENGINE=InnoDB AUTO_INCREMENT='.$_CONFIG['db']['core']['autoincrement'].' DEFAULT CHARSET="'.$_CONFIG['db']['core']['charset'].'" COLLATE="'.$_CONFIG['db']['core']['collate'].'";');
 
-sql_query('CREATE TABLE `authentications` (`id`            INT(11)     NOT NULL AUTO_INCREMENT,
-                                           `createdon`     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                           `createdby`     INT(11)         NULL,
-                                           `status`        VARCHAR(16)     NULL DEFAULT NULL,
-                                           `with_captcha`  INT(1)      NOT NULL DEFAULT NULL,
-                                           `failed_reason` VARCHAR(64)     NULL DEFAULT NULL,
-                                           `users_id`      INT(11)     NOT NULL,
-                                           `ip`            VARCHAR(46),
+sql_query('CREATE TABLE `authentications` (`id`               INT(11)     NOT NULL AUTO_INCREMENT,
+                                           `createdon`        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                           `createdby`        INT(11)         NULL,
+                                           `status`           VARCHAR(16)     NULL DEFAULT NULL,
+                                           `captcha_required` TINYINT(1)  NOT NULL,
+                                           `failed_reason`    VARCHAR(127)    NULL DEFAULT NULL,
+                                           `users_id`         INT(11)         NULL DEFAULT NULL,
+                                           `username`         VARCHAR(64) NOT NULL,
+                                           `ip`               VARCHAR(46),
 
                                            PRIMARY KEY `id`        (`id`),
                                                    KEY `createdon` (`createdon`),
