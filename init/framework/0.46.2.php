f24388cf6154e44250b1523d3fdf2fb8ce9e9010
Juan Jose Munoz Ortega <juan@ingiga.com>
2017-02-20 17:35:07 -0600

    modify column number twilio numbers

---
+++ init/framework/0.46.2.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * twilio numbers modify number of characters in colum number
+ */
+
+sql_index_exists('twilio_numbers', 'number', 'ALTER TABLE `twilio_numbers` DROP INDEX `number`');
+
+sql_column_exists('twilio_numbers', 'number','ALTER TABLE `twilio_numbers` MODIFY `number` varchar(14) NOT NULL');
+
+sql_index_exists('twilio_numbers', 'number', '!ALTER TABLE `twilio_numbers` ADD INDEX `number` (`number`)');
+
+?>
