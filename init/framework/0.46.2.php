fce8fa63c6b9ac56130a573dc1ed8dd1806891e0
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-02-20 17:58:19 -0600

    Cleaned code

--- init/framework/0.46.2.php
+++ init/framework/0.46.2.php
@@ -2,11 +2,8 @@
 /*
  * twilio numbers modify number of characters in colum number
  */
-
-sql_index_exists('twilio_numbers', 'number', 'ALTER TABLE `twilio_numbers` DROP INDEX `number`');
-
-sql_column_exists('twilio_numbers', 'number','ALTER TABLE `twilio_numbers` MODIFY `number` varchar(14) NOT NULL');
-
-sql_index_exists('twilio_numbers', 'number', '!ALTER TABLE `twilio_numbers` ADD INDEX `number` (`number`)');
+sql_index_exists ('twilio_numbers', 'number',  'ALTER TABLE `twilio_numbers` DROP INDEX `number`');
+sql_column_exists('twilio_numbers', 'number',  'ALTER TABLE `twilio_numbers` MODIFY `number` VARCHAR(14) NOT NULL');
+sql_index_exists ('twilio_numbers', 'number', '!ALTER TABLE `twilio_numbers` ADD INDEX `number` (`number`)');
 
 ?>
