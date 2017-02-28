e79c300c1a44b72ac9fba788cb8122f51b4d24f8
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-02-28 03:23:32 -0600

    Improved server validation messages

--- www/en/libs/servers.php
+++ www/en/libs/servers.php
@@ -51,25 +51,25 @@ function servers_validate($server){
          * Validate provider, customer, and ssh account
          */
         if($server['provider']){
-            $server['providers_id'] = sql_get('SELECT `id` FROM `providers`    WHERE `seoname` = :seoname AND `status` IS NULL', array(':seoname' => $server['provider']), 'id');
+            $server['providers_id'] = sql_get('SELECT `id` FROM `providers` WHERE `seoname` = :seoname AND `status` IS NULL', array(':seoname' => $server['provider']), 'id');
 
             if(!$server['providers_id']){
                 $v->setError(tr('servers_validate(): Specified provider ":provider" does not exist', array(':provider' => $server['provider'])));
             }
 
         }else{
-            $v->setError(tr('servers_validate(): No provider specified'));
+            $v->setError(tr('servers_validate(): Please specify a provider'));
         }
 
         if($server['customer']){
-            $server['customers_id'] = sql_get('SELECT `id` FROM `customers`    WHERE `seoname` = :seoname AND `status` IS NULL', array(':seoname' => $server['customer']), 'id');
+            $server['customers_id'] = sql_get('SELECT `id` FROM `customers` WHERE `seoname` = :seoname AND `status` IS NULL', array(':seoname' => $server['customer']), 'id');
 
             if(!$server['customers_id']){
                 $v->setError(tr('servers_validate(): Specified customer ":customer" does not exist', array(':customer' => $server['customer'])));
             }
 
         }else{
-            $v->setError(tr('servers_validate(): No customer specified'));
+            $v->setError(tr('servers_validate(): Please specify a customer'));
         }
 
         if($server['ssh_account']){
