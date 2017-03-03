3d816e8d6c26844b03b0e7e4b9a3353893db0677
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-03-03 04:15:40 -0600

    Fixed init 0.47.1 crash

--- init/framework/0.47.1.php
+++ init/framework/0.47.1.php
@@ -3,5 +3,5 @@
  * Fixed users apikey uniqueness
  */
 sql_index_exists('users', 'apikey', 'ALTER TABLE `users` DROP KEY `apikey`');
-sql_query('ALTER TABLE `users` ADD UNIQUE KEY `url` (`url`)');
+sql_query('ALTER TABLE `users` ADD UNIQUE KEY `apikey` (`apikey`)');
 ?>
