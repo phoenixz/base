ca3d320fd9b6a1a3fc209e3aa517c70514b1c68b
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-04-04 02:37:16 -0500

    chat iframe will now use correct domain

--- www/en/libs/chat.php
+++ www/en/libs/chat.php
@@ -61,7 +61,7 @@ function chat_start($user){
         setcookie('username', $user['user_name']    , time() + 86400, '/', ''.str_starts($_SESSION['domain'], '.'));
         setcookie('password', $user['user_password'], time() + 86400, '/', ''.str_starts($_SESSION['domain'], '.'));
 
-        return '<iframe src="'.$_CONFIG['protocol'].'chat.'.$_SESSION['domain'].'" frameborder="0" class="chat"></iframe>';
+        return '<iframe src="'.$_CONFIG['protocol'].'chat.'.$_CONFIG['domain'].'" frameborder="0" class="chat"></iframe>';
 
     }catch(Exception $e){
         throw new bException(tr('chat_start(): Failed'), $e);
