15dae76a476c208b22194f81e64609837e3e4649
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-04-11 23:10:33 -0500

    FV0.47.4 init file now also fixes blogs_media hash column

--- init/framework/0.47.4.php
+++ init/framework/0.47.4.php
@@ -1,7 +1,35 @@
 <?php
 /*
+ * Fixed blog_media hash column
  * Add "level" to blogs posts, which will be used as previous priority column
  */
+sql_query('ALTER TABLE `blogs_media` CHANGE COLUMN `hash` `hash` VARCHAR(64) NOT NULL');
+
+$medias = sql_query('SELECT `id`, `file` FROM `blogs_media`');
+$update = sql_prepare('UPDATE `blogs_media` SET `hash` = :hash WHERE `id` = :id');
+
+cli_log(tr('Updating all blog media hash values. This might take a little while. NOTE: Each following dot represents one file'));
+
+while($media = sql_fetch($medias)){
+    if(empty($media['file'])) continue;
+    cli_dot(1);
+
+    $hash = '';
+    $file = ROOT.'data/content/photos/'.$media['file'].'-original.jpg';
+
+    if(file_exists($file)){
+        $hash = hash('sha256', $file);
+    }
+
+    if($hash){
+        $update->execute(array(':id'   => $media['id'],
+                               ':hash' => $hash));
+    }
+}
+
+cli_dot(false);
+
+
 sql_column_exists('blogs_posts', 'level', '!ALTER TABLE `blogs_posts` ADD COLUMN `level` INT(11) NOT NULL AFTER `priority`');
 sql_index_exists ('blogs_posts', 'level', '!ALTER TABLE `blogs_posts` ADD INDEX  `level` (`level`)');
 
