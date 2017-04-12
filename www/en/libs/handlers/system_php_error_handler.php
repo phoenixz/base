a7253fff19831f1a024a0b5a525413e597ca5bcc
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-04-11 22:40:44 -0500

    Removed debug code

--- www/en/libs/handlers/system_php_error_handler.php
+++ www/en/libs/handlers/system_php_error_handler.php
@@ -1,12 +1,7 @@
 <?php
-show($errstr);
-debug(true);
-
-show('aaaaaaaaaaaaaaaaaaaaaaaaaaa');
 $session   = "\n\n\n<br><br>SESSION DATA<br><br>\n\n\n".htmlentities(print_r(variable_zts_safe(isset_get($_SESSION)), true));
 $server    = "\n\n\n<br><br>SERVER DATA<br><br>\n\n\n".htmlentities(print_r(variable_zts_safe(isset_get($_SERVER)), true));
 $trace     = "\n\nFUNCTION TRACE\n".htmlentities(var_export(variable_zts_safe(debug_trace()), true));
-show('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
 
 notify('error', '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session.$trace.'</pre>');
 
