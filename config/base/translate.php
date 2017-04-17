cd744b211ea6e805702c4bcb65c605bd70fb346e
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-04-17 02:47:09 -0500

    Fixed toolkit translation API URL

--- config/base/translate.php
+++ config/base/translate.php
@@ -9,7 +9,7 @@
                      most                 (same as above but doesnt fail if not translations are found)
                      none                 (leave original strings if not translations are found)
  */
-$_CONFIG['translate'] = array('url'          => 'http://toolkit.ingiga.com/api/translate.php',
+$_CONFIG['translate'] = array('url'          => 'https://toolkit.ingiga.com/api/translate.php',
                               'mode'         => 'full',
                               'passphrase'   => 'translate',
                               'api_key'      => '',
