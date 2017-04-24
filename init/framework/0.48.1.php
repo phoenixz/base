2fceed7bb8b67dc660d41e4ae1e8a407a5e3af4c
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-04-24 12:12:12 -0500

    Fixed init 0.48.1 SQL crash

--- init/framework/0.48.1.php
+++ init/framework/0.48.1.php
@@ -2,6 +2,6 @@
 /*
  * Fix various possible blog DB issues
  */
-sql_query('DELETE FROM `blogs_posts` WHERE `status` = "_new"');
+sql_query('DELETE FROM `blogs_posts` WHERE `status` = "_new" AND `masters_id` IS NULL');
 sql_query('UPDATE `blogs_posts` SET `masters_id` = `id` WHERE `masters_id` IS NULL');
 ?>
