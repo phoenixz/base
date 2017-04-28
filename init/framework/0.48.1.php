1c7deedfb1b95923a04d59e1c215c46356124c9f
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-04-27 20:48:21 -0500

    Fixed FV0.48.1 init sql crash
    Rows that had masters_id pointed to rows that would be deleted caused foreign key error on some projects

--- init/framework/0.48.1.php
+++ init/framework/0.48.1.php
@@ -2,6 +2,15 @@
 /*
  * Fix various possible blog DB issues
  */
+sql_query('DELETE      `blogs_posts`
+           FROM        `blogs_posts`
+
+           LEFT JOIN   `blogs_posts` AS `siblings`
+           ON          `siblings`.`masters_id` = `blogs_posts`.`id`
+
+           WHERE       `blogs_posts`.`masters_id` IS NOT NULL
+           AND         `siblings`.`masters_id`    IS NULL');
+
 sql_query('DELETE FROM `blogs_posts` WHERE `status` = "_new" AND `masters_id` IS NULL');
 sql_query('UPDATE `blogs_posts` SET `masters_id` = `id` WHERE `masters_id` IS NULL');
 ?>
