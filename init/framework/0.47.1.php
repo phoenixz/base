1f76d41aa1f56b4de6fbcea57c4379bdd7ae62b5
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-05-02 01:16:48 -0500

    Added some extra fixes for FV0.47.1

--- init/framework/0.47.1.php
+++ init/framework/0.47.1.php
@@ -2,6 +2,9 @@
 /*
  * Fixed users apikey uniqueness
  */
+sql_column_exists('users', 'key'   , '!ALTER TABLE `users` ADD COLUMN `key`    VARCHAR(255) NULL AFTER `status`');
+sql_column_exists('users', 'apikey', '!ALTER TABLE `users` ADD COLUMN `apikey` VARCHAR(64)  NULL AFTER `key`');
+
 sql_index_exists('users', 'apikey', 'ALTER TABLE `users` DROP KEY `apikey`');
 sql_query('ALTER TABLE `users` ADD UNIQUE KEY `apikey` (`apikey`)');
 ?>
