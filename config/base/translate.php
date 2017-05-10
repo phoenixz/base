2df6fe813a3ae68f49618f95bd3826b392f3d934
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-05-10 04:47:22 -0500

    Cleaned configuration code, fixed default translation URL

--- config/base/translate.php
+++ config/base/translate.php
@@ -9,10 +9,10 @@
                      most                 (same as above but doesnt fail if not translations are found)
                      none                 (leave original strings if not translations are found)
  */
-$_CONFIG['translate'] = array('url'          => 'https://toolkit.ingiga.com/api/translate.php',
-                              'mode'         => 'full',
-                              'passphrase'   => 'translate',
-                              'api_key'      => '',
-                              'allowed_tags' => '<br><a><strong><span><small><b><i><u>');
+$_CONFIG['translate']                                                           = array('url'          => 'https://toolkit.ingiga.com/api/translate',
+                                                                                        'mode'         => 'full',
+                                                                                        'passphrase'   => 'translate',
+                                                                                        'api_key'      => '',
+                                                                                        'allowed_tags' => '<br><a><strong><span><small><b><i><u>');
 
 ?>
