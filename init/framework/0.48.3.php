51550a126c9fa61485e2954935af49101926a991
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-05-19 22:33:10 -0500

    FV0.48.3: Fixed user account verification issues

--- init/framework/0.48.3.php
+++ init/framework/0.48.3.php
@@ -2,8 +2,12 @@
 /*
  * Update user verification
  */
-sql_column_exists('users', 'validated'     , 'ALTER TABLE `users` CHANGE COLUMN `validated`      `verify_code` VARCHAR(64) NULL');
-sql_column_exists('users', 'date_validated', 'ALTER TABLE `users` CHANGE COLUMN `date_validated` `verifiedon`  DATETIME    NULL');
+sql_column_exists('users', 'validated'     , 'ALTER TABLE `users` CHANGE COLUMN `validated`      `verify_code` VARCHAR(128) NULL');
+sql_column_exists('users', 'date_validated', 'ALTER TABLE `users` CHANGE COLUMN `date_validated` `verifiedon`  DATETIME     NULL');
 
 sql_table_exists('registrations', 'DROP TABLE `registrations`');
+
+sql_query('ALTER TABLE `users` MODIFY COLUMN `verify_code` VARCHAR(128) NULL');
+
+sql_index_exists('users', 'verify_code', '!ALTER TABLE `users` ADD UNIQUE KEY `verify_code` (`verify_code`)');
 ?>
