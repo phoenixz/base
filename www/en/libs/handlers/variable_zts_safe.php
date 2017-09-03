1bf5141aa4ebcec05edffb2bd5dfd49436a80553
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-09-03 16:16:42 -0500

    Fixed show() not displaying Error objects

--- www/en/libs/handlers/variable_zts_safe.php
+++ www/en/libs/handlers/variable_zts_safe.php
@@ -14,7 +14,7 @@ if(is_resource($variable)){
     return '*** Variable is resource ***';
 }
 
-if(is_array($variable) or (is_object($variable) and ($variable instanceof Exception))){
+if(is_array($variable) or (is_object($variable) and (($variable instanceof Exception) or ($variable instanceof Error)))){
     foreach($variable as $key => &$value){
         if($key === 'object'){
             return '*** Not showing objects due to possible segfaults ***';
