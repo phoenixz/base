10bef5b8d4948cdb2c755bd188eb559fe781b34c
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-10-25 17:19:05 -0500

    Fixed query issues in FV0.48.3 init file

--- init/framework/0.48.3.php
+++ init/framework/0.48.3.php
@@ -2,8 +2,11 @@
 /*
  * Update user verification
  */
-sql_column_exists('users', 'validated'     , 'ALTER TABLE `users` CHANGE COLUMN `validated`      `verify_code` VARCHAR(128) NULL');
-sql_column_exists('users', 'date_validated', 'ALTER TABLE `users` CHANGE COLUMN `date_validated` `verifiedon`  DATETIME     NULL');
+sql_query('UPDATE `users` SET `verify_code` = NULL WHERE `verify_code`');
+
+sql_column_exists('users', 'validated'     ,  'ALTER TABLE `users` CHANGE COLUMN `validated`      `verify_code` VARCHAR(128) NULL');
+sql_column_exists('users', 'date_validated',  'ALTER TABLE `users` CHANGE COLUMN `date_validated` `verifiedon`  DATETIME     NULL');
+sql_column_exists('users', 'credits'       , '!ALTER TABLE `users` ADD    COLUMN `credits`                      DOUBLE(7,2)  NULL');
 
 sql_table_exists('registrations', 'DROP TABLE `registrations`');
 
