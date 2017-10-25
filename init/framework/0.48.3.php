1e05ca03e0c9bb5239ce8833cee5ab4db9d4aaf4
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-10-25 18:19:11 -0500

    Fixed FV0.48.3 init crash

--- init/framework/0.48.3.php
+++ init/framework/0.48.3.php
@@ -2,15 +2,14 @@
 /*
  * Update user verification
  */
-sql_query('UPDATE `users` SET `verify_code` = NULL WHERE `verify_code`');
-
 sql_column_exists('users', 'validated'     ,  'ALTER TABLE `users` CHANGE COLUMN `validated`      `verify_code` VARCHAR(128) NULL');
 sql_column_exists('users', 'date_validated',  'ALTER TABLE `users` CHANGE COLUMN `date_validated` `verifiedon`  DATETIME     NULL');
 sql_column_exists('users', 'credits'       , '!ALTER TABLE `users` ADD    COLUMN `credits`                      DOUBLE(7,2)  NULL');
 
 sql_table_exists('registrations', 'DROP TABLE `registrations`');
 
-sql_query('ALTER TABLE `users` MODIFY COLUMN `verify_code` VARCHAR(128) NULL');
+sql_column_exists('users', 'verify_code', 'ALTER TABLE `users` ADD COLUMN `verify_code` VARCHAR(128) NULL');
+sql_query('UPDATE `users` SET `verify_code` = NULL WHERE `verify_code`');
 
 sql_index_exists('users', 'verify_code', '!ALTER TABLE `users` ADD UNIQUE KEY `verify_code` (`verify_code`)');
 ?>
