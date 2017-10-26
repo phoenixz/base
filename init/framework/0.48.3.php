b8da85a00e5f0f262e6954c496578aed450f6fae
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-10-25 19:43:40 -0500

    Fixed FV 0.48.3 for realsies now

--- init/framework/0.48.3.php
+++ init/framework/0.48.3.php
@@ -5,10 +5,10 @@
 sql_column_exists('users', 'validated'     ,  'ALTER TABLE `users` CHANGE COLUMN `validated`      `verify_code` VARCHAR(128) NULL');
 sql_column_exists('users', 'date_validated',  'ALTER TABLE `users` CHANGE COLUMN `date_validated` `verifiedon`  DATETIME     NULL');
 sql_column_exists('users', 'credits'       , '!ALTER TABLE `users` ADD    COLUMN `credits`                      DOUBLE(7,2)  NULL');
+sql_column_exists('users', 'verify_code'   , '!ALTER TABLE `users` ADD    COLUMN `verify_code`                  VARCHAR(128) NULL');
 
 sql_table_exists('registrations', 'DROP TABLE `registrations`');
 
-sql_column_exists('users', 'verify_code', 'ALTER TABLE `users` ADD COLUMN `verify_code` VARCHAR(128) NULL');
 sql_query('UPDATE `users` SET `verify_code` = NULL WHERE `verify_code`');
 
 sql_index_exists('users', 'verify_code', '!ALTER TABLE `users` ADD UNIQUE KEY `verify_code` (`verify_code`)');
