b1172d4e041771575777c9997851929771b6f19f
Sven Oostenbrink <so.oostenbrink@gmail.com>
2017-12-18 22:21:50 -0600

    Fixed init crash on blogs media foreign key

--- init/framework/0.48.1.php
+++ init/framework/0.48.1.php
@@ -11,6 +11,28 @@ sql_query('DELETE      `blogs_posts`
            WHERE       `blogs_posts`.`masters_id` IS NOT NULL
            AND         `siblings`.`masters_id`    IS NULL');
 
+$delete = sql_prepare('DELETE FROM `blogs_media` WHERE `id` = :id');
+$files  = sql_query('SELECT `blogs_media`.`id`,
+                            `blogs_media`.`file`
+
+                     FROM   `blogs_media`
+
+                     JOIN   `blogs_posts`
+                     ON     `blogs_posts`.`id`     = `blogs_media`.`blogs_posts_id`
+
+                     WHERE  `blogs_posts`.`status` = "_new"
+                     AND    `blogs_posts`.`masters_id` IS NULL');
+
+if($files->rowCount()){
+    While($file = sql_fetch($files)){
+        cli_dot();
+        file_delete($file['file']);
+        $delete->execute(array(':id' => $file['id']));
+    }
+
+    cli_dot(false);
+}
+
 sql_query('DELETE FROM `blogs_posts` WHERE `status` = "_new" AND `masters_id` IS NULL');
 sql_query('UPDATE `blogs_posts` SET `masters_id` = `id` WHERE `masters_id` IS NULL');
 ?>
