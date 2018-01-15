261b56605818a26b1343605f223e3d56aa94d453
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-01-14 21:01:39 -0600

    Improved 0.55.0 init

--- init/framework/0.55.0.php
+++ init/framework/0.55.0.php
@@ -5,5 +5,6 @@
  * Currently there are 2 fields for api keys "api_key" and "apikey"
  * The one that is being used is "apikey" so we must delete the field "api_key"
  */
+sql_query('UPDATE `users` SET `apikey` = `api_key` WHERE `apikey` IS NULL AND `api_key` IS NOT NULL');
 sql_query('ALTER TABLE `users` DROP COLUMN `api_key`');
 ?>
