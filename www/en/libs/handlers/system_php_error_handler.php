497b76e1b3c4c356509c65d02d82efe7cab34ebd
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-01-18 20:05:04 -0600

    PHP errors are now always also logged to the base data/log/php-errors file

--- www/en/libs/handlers/system_php_error_handler.php
+++ www/en/libs/handlers/system_php_error_handler.php
@@ -7,6 +7,7 @@ notify('error', '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$
 
 if(PLATFORM_HTTP){
     error_log('PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"');
+    log_file('PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"', 'php-errors');
 }
 
 throw new bException('PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"', $errno);
