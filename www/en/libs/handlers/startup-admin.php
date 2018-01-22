a06a176f079e6efced564f134be253509dc9bb34
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-01-22 16:54:05 -0600

    The admin startup will now always automatically load the atlant library

--- www/en/libs/handlers/startup-admin.php
+++ www/en/libs/handlers/startup-admin.php
@@ -57,7 +57,7 @@ define('ENVIRONMENT', $env);
  * Load cache libraries (done until here since these need configuration @ load time)
  */
 load_config(' ');
-load_libs('http,html,inet,cache'.(empty($_CONFIG['memcached']) ? '' : ',memcached').(empty($_CONFIG['cdn']['enabled']) ? '' : ',cdn'));
+load_libs('http,html,inet,atlant,cache'.(empty($_CONFIG['memcached']) ? '' : ',memcached').(empty($_CONFIG['cdn']['enabled']) ? '' : ',cdn'));
 
 
 
