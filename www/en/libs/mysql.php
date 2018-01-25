12ffa9107234470b4ecbd381730f9e65e6e53513
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-01-24 20:13:04 -0600

    Added mysqldump() function signature

--- www/en/libs/mysql.php
+++ www/en/libs/mysql.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * PDO library
+ * MySQL library
  *
- * This file contains various functions to access databases over PDO
+ * This library contains various functions to manage mysql databases and servers
  *
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Sven Oostenbrink <support@ingiga.com>
@@ -12,6 +12,34 @@
 
 
 
+/*
+ *
+ */
+function mysql_dump($params){
+    try{
+        array_ensure($params, 'host,user,pass,ssh_server');
+
+        if($params['ssh_server']){
+            /*
+             * Execute this over SSH on a remote server
+             */
+
+        }else{
+            /*
+             * Execute this directly on localhost
+             */
+        }
+
+    }catch(Exception $e){
+        throw new bException(tr('mysql_dump(): Failed'), $e);
+    }
+}
+
+
+
+/*
+ *
+ */
 function mysql_master_replication_setup(){
     try{
 
