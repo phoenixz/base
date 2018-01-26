eb9ad187ced28b9f54d3895efe8a2a55c7b4b886
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-01-26 17:04:51 -0600

    Upgraded admin configuration structure

--- config/base/admin.php
+++ config/base/admin.php
@@ -1,4 +1,8 @@
 <?php
-$_CONFIG['admin'] = array('recover' => tr('Recover password'),
-                          'signup'  => tr('Sign up'));
+$_CONFIG['admin'] = array('signin' => array('contactus'  => '<a href="mailto:support@:domain">'.tr('Contact us').'</a>',
+                                            'copyright'  => '© 2018 <a href="https://:domain/copyright.html">Ingiga</a>',
+                                            'rememberme' => tr('Remember me'),
+                                            'recover'    => tr('Recover password'),
+                                            'signup'     => tr('Sign up'),
+                                            'welcome'    => tr('<strong>Welcome!</strong> Please sign in first.')));
 ?>
