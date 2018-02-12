2d37bcd625f625bb66f3e7eaa1a3350e293ad06e
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-02-12 12:14:24 -0600

    Upgraded scanimage library
    Added scanimage_get(): Returns device information with device options for the specified scanner device string
    
    scanimage_list() will now only list scanners from the drivers database
    
    scanimage_search_devices() will search scanner devices directly from the scanner drivers
    
    scanimage_update_devices() will drop all scanner devices from the drivers database and add the newly found scanners
    
    Added scanimage_validate(): This function will validate the scanimage() parameters with the driver options for the specified scanner device

--- www/en/libs/scanimage.php
+++ www/en/libs/scanimage.php
@@ -15,140 +15,15 @@ load_config('scanimage');
 
 
 /*
- * Scan for an image
+ * Scan image using the scanimage command line program
  *
  * Example command: scanimage --progress  --buffer-size --contrast 50 --gamma 1.8 --jpeg-quality 80 --transfer-format JPEG --mode Color --resolution 300 --format jpeg > test.jpg
  */
 function scanimage($params){
-    global $_CONFIG;
-
     try{
-        array_params($params);
-        array_default($params, 'resolution'     , $_CONFIG['scanimage']['resolution']);
-        array_default($params, 'contrast'       , $_CONFIG['scanimage']['contrast']);
-        array_default($params, 'brightness'     , $_CONFIG['scanimage']['brightness']);
-        array_default($params, 'gamma'          , $_CONFIG['scanimage']['gamma']);
-        array_default($params, 'jpeg_quality'   , $_CONFIG['scanimage']['jpeg_quality']);
-        array_default($params, 'transfer_format', $_CONFIG['scanimage']['transfer_format']);
-        array_default($params, 'mode'           , $_CONFIG['scanimage']['mode']);
-        array_default($params, 'format'         , $_CONFIG['scanimage']['format']);
-        array_default($params, 'file'           , file_temp());
-        array_default($params, 'device'         , null);
-
-        $command = 'scanimage';
-
-        /*
-         * Validate parameters and apply them to $command
-         */
-        if($params['contrast']){
-            if(!is_natural($params['contrast']) or ($params['contrast']) > 100){
-                throw new bException(tr('scanimage(): Specified contrast ":value" is invalid. Please ensure the contrast is in between 0 and 100', array(':value' => $params['contrast'])), 'invalid');
-            }
-
-            $command .= ' --contrast '.$params['contrast'];
-        }
-
-        if($params['brightness']){
-            if(!is_natural($params['brightness']) or ($params['brightness']) > 100){
-                throw new bException(tr('scanimage(): Specified brightness ":value" is invalid. Please ensure the brightness is in between 0 and 100', array(':value' => $params['brightness'])), 'invalid');
-            }
-
-            $command .= ' --brightness '.$params['brightness'];
-        }
-
-        if($params['gamma']){
-            if(!is_natural($params['gamma']) or ($params['gamma']) > 100){
-                throw new bException(tr('scanimage(): Specified gamma ":value" is invalid. Please ensure the gamma is in between 0 and 100', array(':value' => $params['gamma'])), 'invalid');
-            }
-
-            $command .= ' --gamma '.$params['gamma'];
-        }
-
-        if($params['resolution']){
-            switch($params['resolution']){
-                case '75':
-                case '150':
-                case '300':
-                case '600':
-                case '1200':
-                case '2400':
-                case '4800':
-                case '9600':
-                    break;
-
-                default:
-                    throw new bException(tr('scanimage(): Specified resolution ":value" is invalid. Please ensure the resolution is one of 75, 150, 150, 300, 600 or 1200', array(':value' => $params['resolution'])), 'invalid');
-            }
-
-            $command .= ' --resolution '.$params['resolution'];
-        }
-
-        if($params['transfer_format']){
-            switch($params['transfer_format']){
-                case 'jpeg':
-                    // FALLTHROUGH
-                case 'tiff':
-                    break;
-
-                default:
-                    throw new bException(tr('scanimage(): Specified transfer_format ":value" is invalid. Please ensure transfer_format is one of JPEG or TIFF', array(':value' => $params['transfer_format'])), 'invalid');
-            }
-
-            $command .= ' --transfer-format '.strtoupper($params['transfer_format']);
-        }
-
-        if($params['format']){
-            switch($params['format']){
-                case 'pnm':
-                    $extension = '.pnm';
-                    break;
-
-                case 'tiff':
-                    $extension = '.tiff';
-                    break;
-
-                case 'jpeg':
-                    $params['format'] = 'tiff';
-                    $extension = '.jpg';
-                    $jpg = true;
-                    break;
-
-                default:
-                    throw new bException(tr('scanimage(): Specified format ":value" is invalid. Please ensure transfer_format is one of JPEG or TIFF', array(':value' => $params['format'])), 'invalid');
-            }
-
-            $command .= ' --format '.$params['format'];
-        }
-
-        if($params['mode']){
-            switch($params['mode']){
-                case 'lineart':
-                    // FALLTHROUGH
-                case 'gray':
-                    // FALLTHROUGH
-                case 'color':
-                    break;
-
-                default:
-                    throw new bException(tr('scanimage(): Specified mode ":value" is invalid. Please ensure mode is one of "color" or "grey" or "lineart"', array(':value' => $params['mode'])), 'invalid');
-            }
-
-            $command .= ' --mode '.str_capitalize($params['mode']);
-        }
-
-        if(!$params['file']){
-            throw new bException(tr('scanimage(): No file specified'), 'invalid');
-        }
-
-        /*
-         * Ensure file ends with correct extension
-         */
-        $len = strlen($extension);
-
-        if(substr($params['file'], -$len, $len) != $extension){
-            $params['file'] .= $extension;
-        }
-
+        $params  = scanimage_validate($params);
+        $command = 'scanimage -d "'.$params['device'].'"'.$device['options'];
+showdie($command);
         /*
          * Finish scan command and execute it
          */
@@ -194,23 +69,150 @@ function scanimage($params){
 
 
 /*
- * List the available scanner devices
+ * Validate the specified scanimage parameters
  */
-function scanimage_list($cached = true){
-    try{
-        if($cached){
-            /*
-             * Get device data from cache
-             */
-            load_libs('drivers');
-            $devices = drivers_get_devices('scanner');
+function scanimage_validate($params){
+    global $_CONFIG;
 
-            if($devices){
-                $devices = sql_list($devices);
-                return $devices;
+    try{
+        load_libs('validate');
+        $v       = new validate_form($params, 'device,jpeg_quality,format,file,buffer_size,options');
+        $options = array();
+
+        /*
+         * Get the device with the device options list
+         */
+        if($params['device']){
+            $device = scanimage_get($params['device']);
+
+        }else{
+            $device = scanimage_get_default();
+
+            if(!$device){
+                $v->setError(tr('No scanner specified and no default scanner found'));
             }
         }
 
+        $options[] = '--device "'.$device['string'].'"';
+
+        /*
+         * Validate target file
+         */
+        if(!$params['file']){
+            $v->setError(tr('scanimage(): No file specified'));
+
+        }elseif(file_exists($params['file'])){
+            $v->setError(tr('scanimage(): Specified file ":file" already exists', array(':file' => $params['file'])), 'exists');
+
+        }else{
+            file_ensure_path(dirname($params['file']));
+        }
+
+        /*
+         * Validate scanner buffer size
+         */
+        if($params['buffer_size']){
+            $v->isNatural($params['buffer_size'], tr('Please specify a valid natural numbered buffer size'));
+            $v->isBetween($params['buffer_size'], 1, 1024, tr('Please specify a valid buffer size between 1 and 1024'));
+        }
+
+        /*
+         * Ensure requested format is known and file ends with correct extension
+         */
+        switch($params['format']){
+            case 'jpeg':
+                $extension = 'jpg';
+                break;
+
+            case 'tiff':
+                $extension = 'tiff';
+                break;
+
+            default:
+                $v->setError(tr('scanimage(): Unknown format ":format" specified', array(':format' => $params['format'])));
+        }
+
+        if(!empty($extension)){
+            if(str_rfrom($params['file'], '.') != $extension){
+                $v->setError(tr('scanimage(): Specified file ":file" has an incorrect file name extension for the requested format ":format", it should have the extension ":extension"', array(':file' => $params['file'], ':format' => $params['format'], ':extension' => $extension)));
+            }
+        }
+
+        /*
+         * Validate parameters against the device
+         */
+        if(!is_array($params['options'])){
+            $v->setError(tr('Please ensure options are specified as an array'));
+
+        }else{
+            foreach($params['options'] as $key => $value){
+                if(!isset($device['options'][$key])){
+                    $v->setError(tr('Driver option ":key" is not supported by device ":device"', array(':option' => $key, ':device' => $params['device'])));
+                    continue;
+                }
+
+                if(is_string($device['options'][$key])){
+                    /*
+                     * This is a value range
+                     */
+                    $device['options'][$key] = array('min' => str_until($key, '..'),
+                                                     'max' => str_from($key, '..'));
+
+                    $v->isNatural($value, tr('Please specify a numeric contrast value'));
+                    $v->isBetween($value, $device['options'][$key]['min'], $device['options'][$key]['max'], tr('Please ensure that ":key" is in between ":min" and ":max"', array(':key' => $key, ':min' => $device['options'][$key]['min'], ':max' => $device['options'][$key]['max'])));
+
+                }else{
+                    $v->inArray($value, $device['options'][$key], tr('Please select a valid ":key" value', array(':key' => $key)));
+                }
+
+                $options[] = '--key "'.$value.'"';
+            }
+        }
+
+        $v->isValid();
+
+        $params['options'] = implode(' ', $options);
+
+        return $params;
+
+    }catch(Exception $e){
+        throw new bException('scanimage_validate(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * List the available scanner devices from the driver database
+ */
+function scanimage_list($device = null, $cached = true){
+    try{
+        /*
+         * Get device data from cache
+         */
+        load_libs('drivers');
+        $devices = drivers_get_devices('scanner', $device);
+
+        if($devices){
+            $devices = sql_list($devices);
+            return $devices;
+        }
+
+        return null;
+
+    }catch(Exception $e){
+        throw new bException('scanimage_list(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Search devices from the scanner drivers. This might take a while, easily up
+ * to 30 seconds or more
+ */
+function scanimage_search_devices(){
+    try{
         $scanners = safe_exec('scanimage -L -q');
         $devices  = array();
 
@@ -238,7 +240,7 @@ function scanimage_list($cached = true){
         return $devices;
 
     }catch(Exception $e){
-        throw new bException('scanimage_list(): Failed', $e);
+        throw new bException('scanimage_search_devices(): Failed', $e);
     }
 }
 
@@ -247,12 +249,12 @@ function scanimage_list($cached = true){
 /*
  * List the available scanner devices
  */
-function scanimage_register_devices(){
+function scanimage_update_devices(){
     try{
         load_libs('drivers');
         drivers_clear_devices('scanner');
 
-        $scanners = scanimage_list(false);
+        $scanners = scanimage_search_devices();
 
         foreach($scanners as $scanner){
             $options = scanimage_get_scanner_details($scanner['device_string']);
@@ -266,7 +268,7 @@ function scanimage_register_devices(){
         return $scanners;
 
     }catch(Exception $e){
-        throw new bException('scanimage_register_devices(): Failed', $e);
+        throw new bException('scanimage_update_devices(): Failed', $e);
     }
 }
 
@@ -413,6 +415,28 @@ function scanimage_get_default(){
         }
 
         return null;
+
+    }catch(Exception $e){
+        throw new bException('scanner_get_default(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Return the data on the default scanner
+ */
+function scanimage_get($device_string){
+    try{
+        load_libs('drivers');
+        $scanner = drivers_get_device($device_string);
+
+        if(!$scanner){
+            throw new bException(tr('scanner_get(): Specified scanner with device string ":string" does not exist', array(':string' => $device_string)), 'not-exist');
+        }
+
+        $scanner['options'] = drivers_get_options($scanner['id']);
+        return $scanner;
 
     }catch(Exception $e){
         throw new bException('scanner_get_default(): Failed', $e);
