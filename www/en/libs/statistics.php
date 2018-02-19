ce718a8446820f4e1e458b0f5b4e79b921b27ee2
ismaharo <ismaharo18@gmail.com>
2018-02-19 17:23:14 -0600

    Updated statistics_add function for new fields

--- www/en/libs/statistics.php
+++ www/en/libs/statistics.php
@@ -13,15 +13,39 @@
 /*
  * Add a new statistical item
  */
-function statistics_add($event, $details = ''){
+function statistics_add($params, $event, $details = ''){
     try{
-        sql_query('INSERT INTO `statistics` (`createdby`, `remote`, `event`, `details`)
-                   VALUES                   (:createdby , :remote , :event , :details )',
+        array_params($params);
+        array_default($params, 'event'    , '');
+        array_default($params, 'details'  , '');
+        array_default($params, 'resource1', null);
+        array_default($params, 'resource2', null);
+
+        if(empty($params['event'])){
+            throw new bException(tr('statistics_add(): No event specified'), 'not-specified');
+        }
+
+        if(empty($params['details'])){
+            throw new bException(tr('statistics_add(): No details specified'), 'not-specified');
+        }
+
+        if(empty($params['resource1'])){
+            throw new bException(tr('statistics_add(): No resource1 specified'), 'not-specified');
+        }
+
+        if($params['resource2'] and empty($params['resource1'])){
+            throw new bException(tr('statistics_add(): No resource1 specified, error caused because of resource2 was specified'), 'not-specified');
+        }
+
+        sql_query('INSERT INTO `statistics` (`createdby`, `remote`, `event`, `details`, `resource1`, `resource2`)
+                   VALUES                   (:createdby , :remote , :event , :details , :resource1 , :resource2 )',
 
                    array(':createdby' => isset_get($_SESSION['user']['id']),
                          ':remote'    => (PLATFORM_HTTP ? $_SERVER['REMOTE_ADDR'] : 'CLI'),
                          ':event'     => $event,
-                         ':details'   => $details));
+                         ':details'   => $details,
+                         ':resource1' => $resource1,
+                         ':resource2' => $resource2));
 
     }catch(Exception $e){
         throw new bException('statistics_add(): Failed', $e);
