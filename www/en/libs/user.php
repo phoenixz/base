d119d1fe1dae91380b08b89f284e70bb8e869e64
Sven Oostenbrink <so.oostenbrink@gmail.com>
2018-02-19 23:24:56 -0600

    Removed obsolete user_process_signin_fields()
    user_process_signin_fields() is obsolete and replaced by html_translate()

--- www/en/libs/user.php
+++ www/en/libs/user.php
@@ -1460,52 +1460,52 @@ function user_switch($users_id, $redirect = '/'){
 
 
 
-/*
- * Find the password field when browser password saving has been disabled
- */
-function user_process_signin_fields($post){
-    global $_CONFIG;
-
-    try{
-        if(empty($_CONFIG['security']['signin']['save_password'])){
-            /*
-             * Clear username and password fields, to ensure they are not being used
-             */
-            unset($post['username']);
-            unset($post['password']);
-
-            /*
-             * Password field is password********
-             */
-            foreach(array_max($post) as $key => $value){
-                if((substr($key, 0, 8) == 'password') and (strlen($key) == 16)){
-                    /*
-                     * This is the password field, set it.
-                     */
-                    $post['password'] = $post[$key];
-                    unset($post[$key]);
-
-                }elseif((substr($key, 0, 8) == 'username') and (strlen($key) == 16)){
-                    /*
-                     * This is the username field, set it.
-                     */
-                    $post['username'] = $post[$key];
-                    unset($post[$key]);
-                    continue;
-                }
-
-                if(isset($post['username']) and isset($post['password'])){
-                    break;
-                }
-            }
-        }
-
-        return $post;
-
-    }catch(Exception $e){
-        throw new bException('user_process_signin_fields(): Failed', $e);
-    }
-}
+///*
+// * Find the password field when browser password saving has been disabled
+// */
+//function user_process_signin_fields($post){
+//    global $_CONFIG;
+//
+//    try{
+//        if(empty($_CONFIG['security']['signin']['save_password'])){
+//            /*
+//             * Clear username and password fields, to ensure they are not being used
+//             */
+//            unset($post['username']);
+//            unset($post['password']);
+//
+//            /*
+//             * Password field is password********
+//             */
+//            foreach(array_max($post) as $key => $value){
+//                if((substr($key, 0, 8) == 'password') and (strlen($key) == 16)){
+//                    /*
+//                     * This is the password field, set it.
+//                     */
+//                    $post['password'] = $post[$key];
+//                    unset($post[$key]);
+//
+//                }elseif((substr($key, 0, 8) == 'username') and (strlen($key) == 16)){
+//                    /*
+//                     * This is the username field, set it.
+//                     */
+//                    $post['username'] = $post[$key];
+//                    unset($post[$key]);
+//                    continue;
+//                }
+//
+//                if(isset($post['username']) and isset($post['password'])){
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return $post;
+//
+//    }catch(Exception $e){
+//        throw new bException('user_process_signin_fields(): Failed', $e);
+//    }
+//}
 
 
 
