38594b2edf3a54e2a1dd4b96f88243eec04865f3
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2018-04-26 13:25:00 -0500

    Added config_write_project()
    Added config_write_project() will write the config/project.php file with the specified variables

--- www/en/libs/config.php
+++ www/en/libs/config.php
@@ -64,4 +64,61 @@ function config_write($section, $environment, $data){
         throw new bException(tr('config_write(): Failed'), $e);
     }
 }
+
+
+
+/*
+ *
+ */
+function config_write_project($project, $project_code_version, $seed){
+    try{
+        $data = '<?php
+/*
+ * Set some very very basic system variables. These are the only "configurable" variables outside
+ * the config file that MUST be edited separately for each project!
+ *
+ * Remember! Base uses two versions, one for the code, and one for the database. These versions are
+ *           used to execute init files to update the system
+ *
+ *
+ * SEED                    Place seed string here or the project won\'t start! Can be just a long
+ *                         string of random characters like "4H%&^}{Jh}{ik9". DO NOT LOSE THIS
+ *                         STRING!
+ *
+ * PROJECT                 Replace the name "BASE" with YOURPROJECTNAME in UPPERCASE. You will have
+ *                         to use YOURPROJECTNAME_ENVIRONMENT in apache (SubEnv
+ *                         YOURPROJECTNAME_ENVIRONMENT development) or bash (export
+ *                         YOURPROJECTNAME_ENVIRONMENT=production) to set require configuration
+ *                         environment
+ *
+ * PROJECTCODEVERSION      Current project version in Major.Minor.Revision format. Up these as you
+ *                         like yourself, and up these once you add new init files
+ *
+ * REQUIRE_SUBENVIRONMENTS If set to true, sub environments will be used. See documentation
+ *                         subenvironments.txt on more information on sub environment, but in short: If you have multiple
+ *                         projects (websites) that basically do the same thing, while using a
+ *                         different data source (database), configuration, CSS interface, etc,
+ *                         then you may want to use sub environments, which allows you to do just
+ *                         that. In Apache you would need to use 2 SetEnv instructions, one for the
+ *                         PROJECTNAME_ENVIRONMENT, and one for the PROJECTNAME_SUBENVIRONMENT. You
+ *                         will need configuration files like production.php (general production
+ *                         configuration), and production_subenvironmentname.php for configuration
+ *                         settings specific to that sub environment
+ *
+ * IMPORTANT!!! DO NOT CHANGE THE SEED VALUE ONCE YOUR PROJECT IS IN USE OR ALL YOUR ACCOUNT AUTHENTICATIONS WILL IRREPARABLY FAIL!
+ *
+ */
+';
+
+        $data .= str_size('define(\'SEED\''              , 33).', \''.$seed.'\');'.CRLF;
+        $data .= str_size('define(\'PROJECT\''           , 33).', \''.$project.'\');'.CRLF;
+        $data .= str_size('define(\'PROJECTCODEVERSION\'', 33).', \''.$project_code_version.'\');'.CRLF;
+        $data .= '?>'.CRLF;
+
+        file_put_contents(ROOT.'config/project.php', $data);
+
+    }catch(Exception $e){
+        throw new bException(tr('config_write_project(): Failed'), $e);
+    }
+}
 ?>
