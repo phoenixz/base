293f140219b9988d00a6a9e5ff5b396a7e4ab5cd
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2018-05-07 12:59:40 -0500

    Fixed init SQL

--- init/framework/1.9.0.php
+++ init/framework/1.9.0.php
@@ -2,5 +2,5 @@
 /*
  *
  */
-sql_column_exists('users', 'webpush', '!ALTER TABLE `users` ADD COLUMN `webpush` VARCHAR(511) NULL;');
+sql_column_exists('users', 'webpush', '!ALTER TABLE `users` ADD COLUMN `webpush` VARCHAR(511) NULL AFTER `timezone`;');
 ?>
