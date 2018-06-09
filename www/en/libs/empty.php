b057f2552d3408648f0725487e555c970f2c1ae2
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2018-06-08 19:19:26 -0500

    Improved empty template library to adhere to new standards

--- www/en/libs/empty.php
+++ www/en/libs/empty.php
@@ -11,7 +11,68 @@
 
 
 /*
- * Empty function
+ * Initialize the library, automatically executed by libs_load()
+ *
+ * NOTE: This function is executed automatically by the load_libs() function and does not need to be called manually
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package
+ *
+ * @return void
+ */
+function empty_library_init(){
+    try{
+        ensure_installed(array('name'      => 'empty',
+                               'project'   => 'emptyear',
+                               'callback'  => 'empty_install',
+                               'checks'    => array(ROOT.'libs/external/empty/')));
+
+    }catch(Exception $e){
+        throw new bException('empty_library_init(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * Install the external empty library
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package
+ *
+ * @param
+ * @return
+ */
+function empty_install($params){
+    try{
+        $params['methods'] = array('apt-get' => array('commands'  => 'sudo apt-get install empty'));
+
+        return install($params);
+
+    }catch(Exception $e){
+        throw new bException('empty_install(): Failed', $e);
+    }
+}
+
+
+
+/*
+ * ...
+ *
+ * @author Sven Olaf Oostenbrink <sven@capmega.com>
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Function reference
+ * @package
+ *
+ * @param
+ * @return
  */
 function empty_function(){
     try{
