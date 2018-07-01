894b58068e533f535b145582907046117949eb2f
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2018-07-01 15:20:28 -0500

    Fixed bytes_convert()
    Added missing function try / catch

--- www/en/libs/numbers.php
+++ www/en/libs/numbers.php
@@ -44,212 +44,217 @@ function bytes($value, $unit = 'auto', $precision = 2) {
  * Convert specified amount explicitly to specified multiplier
  */
 function bytes_convert($amount, $unit = 'auto', $precision = 2, $add_suffix = false){
-    /*
-     * Possibly shift parameters
-     */
-    if(is_bool($precision)){
-        $precision  = 0;
-        $add_suffix = $precision;
-    }
-
-    if(!$amount){
-        $amount = 0;
-    }
-
-    $amount = str_replace(',', '', $amount);
-
-    if(!is_numeric($amount)){
+    try{
         /*
-         * Calculate back to bytes
+         * Possibly shift parameters
          */
-        if(!preg_match('/(\d+(?:\.\d+)?)(\w{1,3})/', $amount, $matches))  {
-            throw new bException('bytes_convert(): Specified amount "'.$amount.'" is not a valid byte amount. Format should be either n, or nKB, nKiB, etc');
+        if(is_bool($precision)){
+            $precision  = 0;
+            $add_suffix = $precision;
         }
 
-        switch(strtolower($matches[2])){
+        if(!$amount){
+            $amount = 0;
+        }
+
+        $amount = str_replace(',', '', $amount);
+
+        if(!is_numeric($amount)){
+            /*
+             * Calculate back to bytes
+             */
+            if(!preg_match('/(\d+(?:\.\d+)?)(\w{1,3})/', $amount, $matches))  {
+                throw new bException('bytes_convert(): Specified amount "'.$amount.'" is not a valid byte amount. Format should be either n, or nKB, nKiB, etc');
+            }
+
+            switch(strtolower($matches[2])){
+                case 'b':
+                    /*
+                     * Just bytes
+                     */
+                    $amount = $matches[1];
+                    break;
+
+                case 'kb':
+                    /*
+                     * Kilo bytes
+                     */
+                    $amount = $matches[1] * 1000;
+                    break;
+
+                case 'kib':
+                    /*
+                     * Kibi bytes
+                     */
+                    $amount = $matches[1] * 1024;
+                    break;
+
+                case 'mb':
+                    /*
+                     * Mega bytes
+                     */
+                    $amount = $matches[1] * 1000000;
+                    break;
+
+                case 'mib':
+                    /*
+                     * Mibi bytes
+                     */
+                    $amount = $matches[1] * 1048576;
+                    break;
+
+                case 'gb':
+                    /*
+                     * Giga bytes
+                     */
+                    $amount = $matches[1] * 1000000 * 1000;
+                    break;
+
+                case 'gib':
+                    /*
+                     * Gibi bytes
+                     */
+                    $amount = $matches[1] * 1048576 * 1024;
+                    break;
+
+                case 'tb':
+                    /*
+                     * Tera bytes
+                     */
+                    $amount = $matches[1] * 1000000 * 1000000;
+                    break;
+
+                case 'tib':
+                    /*
+                     * Tibi bytes
+                     */
+                    $amount = $matches[1] * 1048576 * 1048576;
+                    break;
+
+                default:
+                    throw new bException('bytes_convert(): Specified suffix "'.$suffix.'" on amount "'.$amount.'" is not a valid. Should be one of b, or KB, KiB, mb, mib, etc');
+            }
+        }
+
+        /*
+         * We can only have an integer amount of bytes
+         */
+        $amount = ceil($amount);
+
+        if(strtolower($unit) == 'auto'){
+            /*
+             * Auto determine what unit to use
+             */
+            if($amount > 1048576){
+                if($amount > (1048576 * 1024)){
+                    if($amount > (1048576 * 1048576)){
+                        $unit = 'tb';
+
+                    }else{
+                        $unit = 'gb';
+                    }
+
+                }else{
+                    $unit = 'mb';
+                }
+
+            }else{
+                $unit = 'kb';
+            }
+        }
+
+        /*
+         * Convert to requested unit
+         */
+        switch(strtolower($unit)){
             case 'b':
                 /*
                  * Just bytes
                  */
-                $amount = $matches[1];
                 break;
 
             case 'kb':
                 /*
                  * Kilo bytes
                  */
-                $amount = $matches[1] * 1000;
+                $amount = $amount / 1000;
                 break;
 
             case 'kib':
                 /*
                  * Kibi bytes
                  */
-                $amount = $matches[1] * 1024;
+                $amount = $amount / 1024;
                 break;
 
             case 'mb':
                 /*
                  * Mega bytes
                  */
-                $amount = $matches[1] * 1000000;
+                $amount = $amount / 1000000;
                 break;
 
             case 'mib':
                 /*
                  * Mibi bytes
                  */
-                $amount = $matches[1] * 1048576;
+                $amount = $amount / 1048576;
                 break;
 
             case 'gb':
                 /*
                  * Giga bytes
                  */
-                $amount = $matches[1] * 1000000 * 1000;
+                $amount = $amount / 1000000 / 1000;
                 break;
 
             case 'gib':
                 /*
                  * Gibi bytes
                  */
-                $amount = $matches[1] * 1048576 * 1024;
+                $amount = $amount / 1048576 / 1024;
                 break;
 
             case 'tb':
                 /*
                  * Tera bytes
                  */
-                $amount = $matches[1] * 1000000 * 1000000;
+                $amount = $amount / 1000000 / 1000000;
                 break;
 
             case 'tib':
                 /*
                  * Tibi bytes
                  */
-                $amount = $matches[1] * 1048576 * 1048576;
+                $amount = $amount / 1048576 / 1048576;
                 break;
 
             default:
-                throw new bException('bytes_convert(): Specified suffix "'.$suffix.'" on amount "'.$amount.'" is not a valid. Should be one of b, or KB, KiB, mb, mib, etc');
+                throw new bException('bytes_convert(): Specified unit "'.$unit.'" is not a valid. Should be one of b, or KB, KiB, mb, mib, etc');
         }
-    }
 
-    /*
-     * We can only have an integer amount of bytes
-     */
-    $amount = ceil($amount);
+        $amount = number_format(round($amount, $precision), $precision);
 
-    if(strtolower($unit) == 'auto'){
+        if(!$add_suffix){
+            return $amount;
+        }
+
         /*
-         * Auto determine what unit to use
+         * Return amount with correct suffix.
          */
-        if($amount > 1048576){
-            if($amount > (1048576 * 1024)){
-                if($amount > (1048576 * 1048576)){
-                    $unit = 'tb';
+        switch(strlen($unit)){
+            case 1:
+                return $amount.'b';
 
-                }else{
-                    $unit = 'gb';
-                }
+            case 2:
+                return $amount.strtoupper($unit);
 
-            }else{
-                $unit = 'mb';
-            }
-
-        }else{
-            $unit = 'kb';
+            case 3:
+                return $amount.strtoupper($unit[0]).strtolower($unit[1]).strtoupper($unit[2]);
         }
-    }
 
-    /*
-     * Convert to requested unit
-     */
-    switch(strtolower($unit)){
-        case 'b':
-            /*
-             * Just bytes
-             */
-            break;
-
-        case 'kb':
-            /*
-             * Kilo bytes
-             */
-            $amount = $amount / 1000;
-            break;
-
-        case 'kib':
-            /*
-             * Kibi bytes
-             */
-            $amount = $amount / 1024;
-            break;
-
-        case 'mb':
-            /*
-             * Mega bytes
-             */
-            $amount = $amount / 1000000;
-            break;
-
-        case 'mib':
-            /*
-             * Mibi bytes
-             */
-            $amount = $amount / 1048576;
-            break;
-
-        case 'gb':
-            /*
-             * Giga bytes
-             */
-            $amount = $amount / 1000000 / 1000;
-            break;
-
-        case 'gib':
-            /*
-             * Gibi bytes
-             */
-            $amount = $amount / 1048576 / 1024;
-            break;
-
-        case 'tb':
-            /*
-             * Tera bytes
-             */
-            $amount = $amount / 1000000 / 1000000;
-            break;
-
-        case 'tib':
-            /*
-             * Tibi bytes
-             */
-            $amount = $amount / 1048576 / 1048576;
-            break;
-
-        default:
-            throw new bException('bytes_convert(): Specified unit "'.$unit.'" is not a valid. Should be one of b, or KB, KiB, mb, mib, etc');
-    }
-
-    $amount = number_format(round($amount, $precision), $precision);
-
-    if(!$add_suffix){
-        return $amount;
-    }
-
-    /*
-     * Return amount with correct suffix.
-     */
-    switch(strlen($unit)){
-        case 1:
-            return $amount.'b';
-
-        case 2:
-            return $amount.strtoupper($unit);
-
-        case 3:
-            return $amount.strtoupper($unit[0]).strtolower($unit[1]).strtoupper($unit[2]);
+    }catch(Exception $e){
+        throw new bException(tr('bytes_convert(): Failed'), $e);
     }
 }
 
