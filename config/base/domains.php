87fbe81816d97a423ceb484e291dad01d7a40ba4
Sven Olaf Oostenbrink <sven@capmega.com>
2018-09-24 01:10:28 -0500

    Added default domains library configuration

---
+++ config/base/domains.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * DOMAINS CONFIGURATION FILE
+ *
+ * This configuration file is used by the domains library
+ *
+ * @author Sven Oostenbrink <support@capmega.com>,
+ * @copyright Copyright (c) 2018 Capmega
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @category Configuration
+ * @package
+ */
+$_CONFIG['domains']                                                             = array('scanner' => array('default_tlds' => 'com', 'net', 'org'));
+?>
