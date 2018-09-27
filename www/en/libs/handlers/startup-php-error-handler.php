7fab360a42498ddaca693ff5887d019c89ef2ea2
Sven Olaf Oostenbrink <sven@capmega.com>
2018-09-26 21:37:56 -0500

    Improved PHP error notification

--- www/en/libs/handlers/startup-php-error-handler.php
+++ www/en/libs/handlers/startup-php-error-handler.php
@@ -9,7 +9,9 @@ $session   = "\n\n\n<br><br>SESSION DATA<br><br>\n\n\n".htmlentities(print_r(var
 $server    = "\n\n\n<br><br>SERVER DATA<br><br>\n\n\n".htmlentities(print_r(variable_zts_safe(isset_get($_SERVER)), true));
 $trace     = "\n\nFUNCTION TRACE\n".htmlentities(var_export(variable_zts_safe(debug_trace()), true));
 
-notify('error', '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session.$trace.'</pre>');
+notify(array('title'       => 'php-error',
+             'description' => '<pre> PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"'.$server.$session.$trace.'</pre>',
+             'class'       => 'exception'));
 
 if(PLATFORM_HTTP){
     error_log('PHP ERROR ['.$errno.'] "'.$errstr.'" in "'.$errfile.'@'.$errline.'"');
