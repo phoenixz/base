98b24ccae56f34f4c9e880be442a5f29993da2f4
Sven Olaf Oostenbrink <sven@capmega.com>
2018-09-28 00:49:40 -0500

    Added handlers/startup-script-clean.php
    This is the handler file for script_clean()

---
+++ www/en/libs/handlers/startup-script-clean.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *
+ */
+try{
+    file_delete($script_file, true);
+    log_console(tr('Cleaning up temporary script file ":script"', array(':script' => $script_file)), 'DEBUG/cyan');
+
+}catch(Exception $e){
+    /*
+     * Failed to delete the temporary script file
+     */
+    log_console(tr('script_exec(): Failed to clean up executed temporary script copy path ":file", probably a parrallel process added new content there?', array(':file' => $script_file)), 'yellow');
+}
+?>
