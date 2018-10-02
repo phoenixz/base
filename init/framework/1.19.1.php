22cb543a5033efa916d10fdc4043ee93f2c8e48e
Sven Olaf Oostenbrink <sven@capmega.com>
2018-10-01 20:56:31 -0500

    Fixed FV1.19.1 Added ipv4 updates

--- init/framework/1.19.1.php
+++ init/framework/1.19.1.php
@@ -5,16 +5,36 @@
 sql_column_exists('servers_hostnames', 'seohostname', '!ALTER TABLE `servers_hostnames` ADD COLUMN `seohostname` VARCHAR(64) NOT NULL DEFAULT "" AFTER `hostname`');
 sql_index_exists ('servers_hostnames', 'seohostname', '!ALTER TABLE `servers_hostnames` ADD KEY    `seohostname` (`seohostname`)');
 
-$servers = sql_query('SELECT `id`, `hostname` FROM `servers`');
+$servers = sql_query('SELECT `id`, `hostname`, `ipv4` FROM `servers`');
 $insert  = sql_prepare('INSERT INTO `servers_hostnames` (`meta_id`, `servers_id`, `hostname`, `seohostname`)
                         VALUES                          (:meta_id , :servers_id , :hostname , :seohostname )');
 
 load_libs('seo');
-log_console(tr('Updating server hostnames in multi hostnames list'));
+log_console(tr('Updating server IPv4\'s and server hostnames in multi hostnames list'));
 
 sql_query('TRUNCATE `servers_hostnames`');
 
 while($server = sql_fetch($servers)){
+    if(!$server['ipv4']){
+        $server['ipv4'] = gethostbynamel($server['hostname']);
+
+        if(!$server['ipv4']){
+            $server['ipv4'] = null;
+            log_console(tr('No IPv4 found for hostname ":hostname"', array(':ip' => $server['ipv4'], ':hostname' => $server['hostname'])), 'yellow');
+
+        }else{
+            if(count($server['ipv4']) == 1){
+                $server['ipv4'] = array_shift($server['ipv4']);
+                log_console(tr('Set IPv4 ":ip" for hostname ":hostname"', array(':ip' => $server['ipv4'], ':hostname' => $server['hostname'])));
+
+            }else{
+                log_console(tr('Found multiple IPv4 entries for hostname ":hostname", not automatically updating', array(':hostname' => $server['hostname'])), 'yellow');
+            }
+        }
+
+        sql_query('UPDATE `servers` SET `ipv4` = :ipv4 WHERE `id` = :id', array(':id' => $server['id'], ':ipv4' => $server['ipv4']));
+    }
+
     $servers_id = sql_get('SELECT `servers_id` FROM `servers_hostnames` WHERE `hostname` = :hostname', true, array('hostname' => $server['hostname']));
 
     if($servers_id){
@@ -26,7 +46,7 @@ while($server = sql_fetch($servers)){
         }
 
     }else{
-        cli_dot(1);
+        log_console(tr('Adding hostname ":hostname" to servers hostnames table', array(':hostname' => $server['hostname'])));
         $insert->execute(array(':meta_id'     => meta_action(),
                                ':servers_id'  => $server['id'],
                                ':hostname'    => $server['hostname'],
