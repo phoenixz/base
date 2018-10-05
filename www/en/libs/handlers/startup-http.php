922bf8d0f9bdb190605b5253726ca6ab2e3dcaca
Sven Olaf Oostenbrink <sven@capmega.com>
2018-10-05 14:59:08 -0500

    Added html_fix_checkbox_values()
    This is executed automatically on POST requests
    
    html_fix_checkbox_values will check if there are values available starting with __CBNAME an if keys with NAME are missing. IF so, the __CBNAME will be put in place of the missing NAME keys, and the __CBNAME keys will be deleted

--- www/en/libs/handlers/startup-http.php
+++ www/en/libs/handlers/startup-http.php
@@ -219,6 +219,7 @@ if(isset($_GET['redirect'])){
  */
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     html_untranslate();
+    html_fix_checkbox_values();
 
     if($_CONFIG['security']['csrf']['enabled'] === 'force'){
         /*
