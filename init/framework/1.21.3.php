923e1fed3449143d9ddce478d399081f569cb1ae
Camilo Antonio Rodriguez <crodriguez@capmega.com>
2018-10-17 20:14:01 -0500

    Fixed FV1.21.3 query bug

--- init/framework/1.21.3.php
+++ init/framework/1.21.3.php
@@ -3,6 +3,6 @@
  * Fix servers table, createdby may be NULL
  */
 sql_query('ALTER TABLE `users`
-           CHANGE COLUMN `latitude`  `latitude`  DOUBLE(10, 14) NULL DEFAULT NULL,
-           CHANGE COLUMN `longitude` `longitude` DOUBLE(10, 14) NULL DEFAULT NULL');
+           CHANGE COLUMN `latitude`  `latitude`  DOUBLE(14, 14) NULL DEFAULT NULL,
+           CHANGE COLUMN `longitude` `longitude` DOUBLE(14, 14) NULL DEFAULT NULL');
 ?>
