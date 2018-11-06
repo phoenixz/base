e575841ed619945b89bbf514138d057c297f5e84
Sven Olaf Oostenbrink <sven@capmega.com>
2018-11-06 17:03:31 -0600

    Upgraded audio library
    Added phpdoc commentaries
    
    Added logging
    
    Cleaned code

--- config/base/audio.php
+++ config/base/audio.php
@@ -3,13 +3,13 @@
  * Audio configuration, there are classes of audio that can be played
  * and also there are different commands to play different types of audio.
  */
-$_CONFIG['audio'] = array('classes' => array('alarm'     => 'alarm.mp3',
-                                             'exception' => 'critical.mp3',
-                                             'notify'    => 'ping.mp3',
-                                             'default'   => 'alarm.mp3'),
+$_CONFIG['audio']                                                               = array('classes' => array('alarm'     => 'alarm.mp3',
+                                                                                                           'exception' => 'critical.mp3',
+                                                                                                           'notify'    => 'ping.mp3',
+                                                                                                           'default'   => 'alarm.mp3'),
 
-                          'quiet'   => false,
-                          'command' => 'mplayer',
-                          'default' => 'exception');
+                                                                                        'quiet'   => false,
+                                                                                        'command' => 'mplayer',
+                                                                                        'default' => 'exception');
 
 ?>
