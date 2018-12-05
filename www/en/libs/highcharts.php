069057f1b6cf3b5134db4c21898e97d34e05f89b
Sven Olaf Oostenbrink <sven@capmega.com>
2018-12-04 19:14:46 -0600

    Obsoleted library highcharts
    The only function in this library has now been moved to the graph-highcharts library

--- www/en/libs/highcharts.php
+++ www/en/libs/highcharts.php
@@ -1,4 +1,5 @@
 <?php
+// :OBSOLETE: This library is obsolete, see the library graph-highcharts
 /*
  * highcharts library
  *
@@ -12,56 +13,15 @@
  * $params['data']  => Data to send at server in call ajax
  * $params['class'] => Aditional css classes to chart
  */
+// :OBSOLETE: This library is obsolete, see the library graph-highcharts
 function highchart_line($params){
     try{
-        array_params($params);
-        array_default($params, 'name' , 'container');
-        array_default($params, 'class', 'actual-chart');
-        array_default($params, 'url'  , '/ajax/charts/atlant_chart_demo.php');
-        array_default($params, 'data' , 'x : true');
-
-        html_load_js(array('plugins/morris/raphael-min',
-                           'plugins/owl/owl.carousel',
-                           'plugins/morris/morris',
-                           'plugins/nvd3/lib/d3.v3',
-                           'plugins/nvd3/nv.d3',
-                           'plugins/sparkline/jquery.sparkline',
-                           'plugins',
-                           'actions',
-                           'hasoffers_charts'));
-
-        $html  = '  <div class="'.$params['class'].'">
-                        <div id="'.$params['name'].'" name="'.$params['name'].'"></div>
-                    </div>';
-
-        $html .= html_script('
-            function load_charts(data){
-                charts["'.$params['name'].'"] = Morris.Line({element: data["element"],
-                                                             data   : data["data"],
-                                                             xkey   : data["xkey"],
-                                                             ykeys  : data["ykeys"],
-                                                             labels : data["labels"],
-                                                             resize : data["resize"]});
-            }
-
-            var charts = [];
-
-            $.ajax({
-                url: "'.$params['url'].'",
-                type: "GET",
-                dataType: "JSON",
-                data: {'.$params['data'].'}})
-            .done(function(data) {
-              load_charts(data);
-            })
-            .fail(function(e) {
-                console.log(e);
-            });');
-
-        return $html;
+        $params['type'] = 'line';
+        load_libs('graph-highcharts');
+        obsolete();
+        return graph_highcharts($params);
 
     }catch(Exception $e){
-showdie($e);
         throw new bException('highchart_line(): Failed', $e);
     }
 }
