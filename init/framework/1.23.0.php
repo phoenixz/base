248d2e41ed27c1b5e4fd8f0be37dd628cf7012f8
Sven Olaf Oostenbrink <sven@capmega.com>
2018-12-11 12:37:11 -0600

    Fixed init FV1.23.0
    Fixed various design issues

--- init/framework/1.23.0.php
+++ init/framework/1.23.0.php
@@ -3,10 +3,13 @@
  * Add offset location support for user
  * Add city / state / country location support for users
  */
-sql_column_exists('users', 'offset_latitude' , '!ALTER TABLE `users` ADD COLUMN `offset_latitude`  DECIMAL(18,15) NULL AFTER `longitude`');
+load_libs('user,blogs,geo');
+
+sql_column_exists('users', 'accuracy'        , '!ALTER TABLE `users` ADD COLUMN `accuracy`         INT(11)        NULL AFTER `longitude`');
+sql_column_exists('users', 'offset_latitude' , '!ALTER TABLE `users` ADD COLUMN `offset_latitude`  DECIMAL(18,15) NULL AFTER `accuracy`');
 sql_column_exists('users', 'offset_longitude', '!ALTER TABLE `users` ADD COLUMN `offset_longitude` DECIMAL(18,15) NULL AFTER `offset_latitude`');
 
-sql_column_exists('users', 'cities_id',    '!ALTER TABLE `users` ADD COLUMN `cities_id`    INT(11) NULL AFTER `fake`');
+sql_column_exists('users', 'cities_id',    '!ALTER TABLE `users` ADD COLUMN `cities_id`    INT(11) NULL AFTER `offset_longitude`');
 sql_column_exists('users', 'states_id',    '!ALTER TABLE `users` ADD COLUMN `states_id`    INT(11) NULL AFTER `cities_id`');
 sql_column_exists('users', 'countries_id', '!ALTER TABLE `users` ADD COLUMN `countries_id` INT(11) NULL AFTER `states_id`');
 
@@ -18,26 +21,29 @@ sql_foreignkey_exists('users', 'fk_users_cities_id',    '!ALTER TABLE `users` AD
 sql_foreignkey_exists('users', 'fk_users_states_id',    '!ALTER TABLE `users` ADD CONSTRAINT `fk_users_states_id`    FOREIGN KEY (`states_id`)    REFERENCES `geo_states`    (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT');
 sql_foreignkey_exists('users', 'fk_users_countries_id', '!ALTER TABLE `users` ADD CONSTRAINT `fk_users_countries_id` FOREIGN KEY (`countries_id`) REFERENCES `geo_countries` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT');
 
-sql_foreignkey_exists('users', 'fk_users_countries', '!ALTER TABLE `users` DROP CONSTRAINT `fk_users_countries`');
+sql_foreignkey_exists('users', 'fk_users_countries', 'ALTER TABLE `users` DROP FOREIGN KEY `fk_users_countries`');
 
-$users = sql_query('SELECT `id`, `latitude`, `longitude` FROM `users` WHERE `latitude` IS NO NULL OR `longitude` IS NOT NULL');
+$users = sql_query('SELECT `id`, `latitude`, `longitude` FROM `users` WHERE `latitude` IS NOT NULL OR `longitude` IS NOT NULL');
 
 while($user = sql_fetch($users)){
     /*
      * Validate LAT/LONG. If one is invalid, reset both to NULL
      */
-    $user = geo_validate_location($user);
+    $user = geo_validate($user);
 
     /*
      * Depending on configuration, update offset lat / long
      * If GEO library has been installed, update city, state, and country information
      */
     user_update_location($user);
+    cli_dot();
 }
 
 /*
  * Sync users blog location information
  */
-blogs_sync_location(array('direction' => 'users',
-                          'blogs'     => 'escorts'));
+$blogs_id = sql_query('SELECT `id` FROM `blogs` WHERE `seoname` = "escorts"');
+$posts    = sql_query('SELECT `id` FROM `blogs_posts` WHERE `latitude` IS NOT NULL OR `longitude` IS NOT NULL');
+
+blogs_sync_location();
 ?>
